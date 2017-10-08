2af709edc9b8335b7cd18bbbd2e0a75e6d21e59a
Teapot418 <teapot404@gmail.com>
2017-10-08 15:58:57 +0200

    Template fixes

--- lib/template.h
+++ lib/template.h
@@ -9,7 +9,7 @@ using cmpl  = complex<double>;
 #define IT iterator
 
 #define rep(i, b, e)  for (int i = b; i < (e); i++)
-#define repd(i, b, e) for (int i = int(b)-1; i >= (e); i++)
+#define repd(i, b, e) for (int i = int(b); i >= (e); i--)
 #define each(a, x)    for (auto& a : x)
 #define all(x)        (x).begin(), (x).end()
 #define sz(x)         int((x).size())
