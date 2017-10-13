eb94f6c96bdd29066574d5c5a37f6770e7ae1d7d
Teapot418 <teapot404@gmail.com>
2017-10-13 23:47:55 +0200

    Merge tests

--- tests/structures/pairing_heap.cpp
+++ tests/structures/pairing_heap.cpp
@@ -5,32 +5,50 @@
 int main() {
 	srand(123);
 
-	PHeap<int, greater<int>> heap;
-	priority_queue<int> que;
+	// PHeap<int, greater<int>> heap;
+	// priority_queue<int> que;
 
-	for (int i = 0; i < 1000000; i++) {
-		int tmp = r(-10000, 10000);
-		que.push(tmp);
-		heap.push(tmp);
-	}
+	// for (int i = 0; i < 1000000; i++) {
+	// 	int tmp = r(-10000, 10000);
+	// 	que.push(tmp);
+	// 	heap.push(tmp);
+	// }
 
-	while (true) {
-		assert(que.empty() == heap.empty());
-		int tmp = r(100000, 500000);
 
-		for (int i = 0; i < tmp; i++ ) {
-			// printf("%d %d\n", que.top(), heap.top());
-			assert(que.top() == heap.top());
-			que.pop();
-			heap.pop();
-		}
+	// while (true) {
+	// 	assert(que.empty() == heap.empty());
+	// 	int tmp = r(100000, 500000);
 
-		for (int i = 0; i < tmp; i++) {
+	// 	for (int i = 0; i < tmp; i++ ) {
+	// 		// printf("%d %d\n", que.top(), heap.top());
+	// 		assert(que.top() == heap.top());
+	// 		que.pop();
+	// 		heap.pop();
+	// 	}
+
+	// 	for (int i = 0; i < tmp; i++) {
+	// 		int tmp = r(-10000, 10000);
+	// 		que.push(tmp);
+	// 		heap.push(tmp);
+	// 	}
+	// }
+
+	PHeap<int> heap;
+
+	for (int j = 0; j < 100; j++) {
+		PHeap<int> heap2;
+
+		for (int i = 0; i < 10000; i++) {
 			int tmp = r(-10000, 10000);
-			que.push(tmp);
 			heap.push(tmp);
 		}
+
+		heap.merge(move(heap2));
 	}
 
+	while (!heap.empty()) {
+		// printf("%d\n", heap.top());
+		heap.pop();
+	}
 	return 0;
 }
