41dd0a7e995895ad91e4773d83122bac199ad8ee
Teapot418 <teapot404@gmail.com>
2017-10-13 22:42:13 +0200

    Pairing heap changes

--- lib/structures/pairing_heap.h
+++ lib/structures/pairing_heap.h
@@ -3,16 +3,16 @@
 template<class T, class Cmp = less<T>>
 struct PHeap {
 	struct Node;
-	using NodeP = unique_ptr<Node>;
+	using NodeP = unique_ptr<Node>; // To disable deallocation: change to Node*
 
 	struct Node {
 		T top;
-		NodeP child, next;
+		NodeP child{nullptr}, next{nullptr};
 
 		Node(T x = T()) { top = x; }
 	};
 
-	NodeP root;
+	NodeP root{nullptr};
 
 	NodeP merge(NodeP l, NodeP r) {
 		if (!l) return move(r);
@@ -29,12 +29,13 @@ struct PHeap {
 	NodeP mergePairs(NodeP v) {
 		if (!v || !v->next) return v;
 		NodeP v2 = move(v->next), v3 = move(v2->next);
+		v->next = nullptr; v2->next = nullptr;
 		return merge(merge(move(v), move(v2)), mergePairs(move(v3)));
 	}
 
 	bool     empty()          { return !root; }
 	const T& top()            { assert(root); return root->top; }
 	void     push(const T& x) { root = merge(move(root), NodeP(new Node(x))); }
-	void     merge(PHeap&& r) { root = merge(move(root), move(r.root)); }
+	void     merge(PHeap&& r) { root = merge(move(root), move(r.root)); r.root = nullptr; }
 	void     pop()            { root = mergePairs(move(root->child)); }
 };
--- tests/structures/pairing_heap.cpp
+++ tests/structures/pairing_heap.cpp
@@ -7,28 +7,38 @@ int main() {
 	PHeap<int, greater<int>> heap;
 	// priority_queue<int> que;
 
+	// for (int i = 0; i < 1000000; i++) {
+	// 	int tmp = r(-10000, 10000);
+	// 	// que.push(tmp);
+	// 	heap.push(tmp);
+	// }
+
+	// for (int i = 0; i < 1; i++) {
+	// 	// assert(que.empty() == heap.empty());
+	// 	int tmp = r(100000, 500000);
+
+	// 	for (int i = 0; i < tmp; i++ ) {
+	// 		// printf("%d %d\n", que.top(), heap.top());
+	// 		// assert(que.top() == heap.top());
+	// 		// que.pop();
+	// 		heap.pop();
+	// 	}
+
+	// 	for (int i = 0; i < tmp; i++) {
+	// 		int tmp = r(-10000, 10000);
+	// 		// que.push(tmp);
+	// 		heap.push(tmp);
+	// 	}
+	// }
+
 	for (int i = 0; i < 1000000; i++) {
 		int tmp = r(-10000, 10000);
-		// que.push(tmp);
 		heap.push(tmp);
 	}
 
-	for (int i = 0; i < 1; i++) {
-		// assert(que.empty() == heap.empty());
-		int tmp = r(100000, 500000);
-
-		for (int i = 0; i < tmp; i++ ) {
-			// printf("%d %d\n", que.top(), heap.top());
-			// assert(que.top() == heap.top());
-			// que.pop();
-			heap.pop();
-		}
-
-		for (int i = 0; i < tmp; i++) {
-			int tmp = r(-10000, 10000);
-			// que.push(tmp);
-			heap.push(tmp);
-		}
+	while (!heap.empty()) {
+		// printf("%d\n", heap.top());
+		heap.pop();
 	}
 
 	return 0;
