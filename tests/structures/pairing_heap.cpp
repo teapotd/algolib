c70961a15d89cef5a77d5f000d59738013b15850
Teapot418 <teapot404@gmail.com>
2017-10-13 20:41:07 +0200

    Pairing heap (not working properly yet)

--- tests/structures/pairing_heap.cpp
+++ tests/structures/pairing_heap.cpp
@@ -2,5 +2,34 @@
 #include "../testing.h"
 
 int main() {
+	srand(123);
+
+	PHeap<int, greater<int>> heap;
+	// priority_queue<int> que;
+
+	for (int i = 0; i < 1000000; i++) {
+		int tmp = r(-10000, 10000);
+		// que.push(tmp);
+		heap.push(tmp);
+	}
+
+	for (int i = 0; i < 1; i++) {
+		// assert(que.empty() == heap.empty());
+		int tmp = r(100000, 500000);
+
+		for (int i = 0; i < tmp; i++ ) {
+			// printf("%d %d\n", que.top(), heap.top());
+			// assert(que.top() == heap.top());
+			// que.pop();
+			heap.pop();
+		}
+
+		for (int i = 0; i < tmp; i++) {
+			int tmp = r(-10000, 10000);
+			// que.push(tmp);
+			heap.push(tmp);
+		}
+	}
+
 	return 0;
 }
