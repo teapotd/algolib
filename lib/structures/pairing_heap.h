57c3831d15605a67647db1a0f862a19e09ad445c
Teapot418 <teapot404@gmail.com>
2017-10-14 12:25:26 +0200

    Pairing heap: decrease-key

--- lib/structures/pairing_heap.h
+++ lib/structures/pairing_heap.h
@@ -8,10 +8,23 @@ struct PHeap {
 	using NodeP = unique_ptr<Node>; // To disable deallocation: change to Node* (and use bump allocator)
 
 	struct Node {
-		T top;
+		T val;
 		NodeP child{nullptr}, next{nullptr};
+		Node* prev{nullptr};
 
-		Node(T x = T()) { top = x; }
+		Node(T x = T()) { val = x; }
+
+		void setChild(NodeP v) {
+			if (child && child->prev == this) child->prev = nullptr;
+			child = move(v);
+			if (child) child->prev = this;
+		}
+
+		void setNext(NodeP v) {
+			if (next && next->prev == this) next->prev = nullptr;
+			next = move(v);
+			if (next) next->prev = this;
+		}
 	};
 
 	NodeP root{nullptr};
@@ -20,23 +33,52 @@ struct PHeap {
 		if (!l) return move(r);
 		if (!r) return move(l);
 
-		if (Cmp()(l->top, r->top)) swap(l, r);
+		if (Cmp()(l->val, r->val)) swap(l, r);
 
-		l->next = move(r->child);
-		r->child = move(l);
+		l->setNext(move(r->child));
+		r->setChild(move(l));
 		return move(r);
 	}
 
 	NodeP mergePairs(NodeP v) {
 		if (!v || !v->next) return v;
 		NodeP v2 = move(v->next), v3 = move(v2->next);
+
 		v->next = nullptr; v2->next = nullptr;
+		v->prev = nullptr; v2->prev = nullptr;
+		if (v3) v3->prev = nullptr;
+
 		return merge(merge(move(v), move(v2)), mergePairs(move(v3)));
 	}
 
+	Node* push(const T& x) {
+		NodeP tmp(new Node(x));
+		auto ptr = &*tmp;
+		root = merge(move(root), move(tmp));
+		return ptr;
+	}
+
+	void decrease(Node* v, T val) {
+		assert(!Cmp()(v->val, val));
+		v->val = val;
+
+		if (!v->prev) return;
+		NodeP uniq;
+
+		if (&*v->prev->child == v) {
+			uniq = move(v->prev->child);
+			v->prev->setChild(move(v->next));
+		} else {
+			uniq = move(v->prev->next);
+			v->prev->setNext(move(v->next));
+		}
+
+		uniq->prev = nullptr; uniq->next = nullptr;
+		root = merge(move(root), move(uniq));
+	}
+
 	bool     empty()          { return !root; }
-	const T& top()            { return root->top; }
-	void     push(const T& x) { root = merge(move(root), NodeP(new Node(x))); }
+	const T& top()            { return root->val; }
 	void     merge(PHeap&& r) { root = merge(move(root), move(r.root)); r.root = nullptr; }
 	void     pop()            { root = mergePairs(move(root->child)); }
 };
