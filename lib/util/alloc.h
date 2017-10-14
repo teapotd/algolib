9d5be48addc33700df4ff5efd0e176dcec6ffe2e
Teapot418 <teapot404@gmail.com>
2017-10-14 20:57:58 +0200

    Simplify alloc

--- lib/util/alloc.h
+++ lib/util/alloc.h
@@ -2,8 +2,8 @@
 
 #include "template.h"
 
-static char memPool[512*1024*1024];
-static int memOffset;
+char memPool[512*1024*1024];
+int memOffset;
 
 void* operator new(size_t n) { memOffset += n; return &memPool[memOffset-n]; }
 void  operator delete(void*) {}
