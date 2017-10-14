c27e742020565bf37f9b58b3d6455359d0e0acb3
Teapot418 <teapot404@gmail.com>
2017-10-14 23:54:05 +0200

    Bellman inequalities fix

--- lib/math/bellman_inequalities.h
+++ lib/math/bellman_inequalities.h
@@ -6,8 +6,8 @@ struct Constraint {
 	ll a, b, limit; // a - b >= limit
 };
 
-bool solveIneq(const vector<Constraint>& edges, vector<ll>& vars) {
-	rep(i, 0, sz(vars)) each(e, edges) vars[e.b] = min(vars[e.b], vars[e.a] - vars[e.limit]);
-	each(e, edges) if (vars[e.a] - vars[e.limit] < vars[e.b]) return false;
+bool solveIneq(vector<Constraint>& edges, vector<ll>& vars) {
+	rep(i, 0, sz(vars)) each(e, edges) vars[e.b] = min(vars[e.b], vars[e.a] - e.limit);
+	each(e, edges) if (vars[e.a] - e.limit < vars[e.b]) return false;
 	return true;
 }
