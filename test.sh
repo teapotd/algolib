8c1457db24de7c882e6afedb1bf7d17379ecb51a
Teapot418 <teapot404@gmail.com>
2017-10-14 14:50:33 +0200

    Change bin/ to build/

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 set -e
 ROOT=`pwd`
 
-mkdir -p `dirname $ROOT/bin/tests/$1`
-g++-6 -O2 -Wall -Wextra -std=c++11 -Wl,-stack_size -Wl,16000000 -Wl,-no_pie -o $ROOT/bin/tests/$1 $ROOT/tests/$1.cpp
-time $ROOT/bin/tests/$1
+mkdir -p `dirname $ROOT/build/tests/$1`
+g++-6 -O2 -Wall -Wextra -std=c++11 -Wl,-stack_size -Wl,16000000 -Wl,-no_pie -o $ROOT/build/tests/$1 $ROOT/tests/$1.cpp
+time $ROOT/build/tests/$1
