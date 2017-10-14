69e7212644e04e1b202318bb701288d7b2e98e30
Teapot418 <teapot404@gmail.com>
2017-10-14 12:59:50 +0200

    single_ptr fixes

--- lib/template.h
+++ lib/template.h
@@ -17,6 +17,25 @@ using cmpl  = complex<double>;
 #define sz(x)         int((x).size())
 
 
+// unique_ptr without deallocation
+template<typename T>
+struct single_ptr {
+	T* elem{0};
+
+	single_ptr()                              {}
+	single_ptr(nullptr_t)                     {}
+	single_ptr(T* v)           : elem(v)      {}
+	single_ptr(single_ptr&& r) : elem(r.elem) { r.elem = 0; }
+
+	single_ptr& operator=(nullptr_t)      { elem = 0; return *this; }
+	single_ptr& operator=(single_ptr&& r) { elem = r.elem; r.elem = 0; return *this; }
+
+	T* operator->() { return elem; }
+	T& operator*()  { return *elem; }
+	operator bool() { return elem; }
+};
+
+
 // #!/bin/bash
 // set -e
 
