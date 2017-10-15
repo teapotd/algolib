4fdabe69262ae97a640fa4ecad0abac7dfbcaa0b
Teapot418 <teapot404@gmail.com>
2017-10-15 10:41:07 +0200

    Move single_ptr to alloc.h

--- lib/template.h
+++ lib/template.h
@@ -18,22 +18,3 @@ using cmpl  = complex<double>;
 
 #define gcd           __gcd
 #define popcount      __builtin_popcount
-
-
-// unique_ptr without deallocation
-template<typename T>
-struct single_ptr {
-	T* elem{0};
-
-	single_ptr()                              {}
-	single_ptr(nullptr_t)                     {}
-	single_ptr(T* v)           : elem(v)      {}
-	single_ptr(single_ptr&& r) : elem(r.elem) { r.elem = 0; }
-
-	single_ptr& operator=(nullptr_t)      { elem = 0; return *this; }
-	single_ptr& operator=(single_ptr&& r) { elem = r.elem; r.elem = 0; return *this; }
-
-	T* operator->() { return elem; }
-	T& operator*()  { return *elem; }
-	operator bool() { return elem; }
-};
