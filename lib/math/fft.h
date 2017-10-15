b540fcb8ecef28ba5b545b010752f29eeb14413a
Teapot418 <teapot404@gmail.com>
2017-10-15 13:11:38 +0200

    FFT fix

--- lib/math/fft.h
+++ lib/math/fft.h
@@ -13,12 +13,20 @@ template<bool inv>
 void fft(vector<cmpl>::IT in, vector<cmpl>::IT out, int size, int step = 1) {
 	if (size == 1) { *out = *in; return; }
 
-	fft<inv>(in,      out,        size*2, step*2);
-	fft<inv>(in+step, out+size/2, size*2, step*2);
+	fft<inv>(in,      out,        size/2, step*2);
+	fft<inv>(in+step, out+size/2, size/2, step*2);
 
 	rep(i, 0, size/2) {
 		auto t = out[i], m = bases[(inv ? i : size-i)*step];
 		out[i]        = t + out[i+size/2]*m;
 		out[i+size/2] = t - out[i+size/2]*m;
 	}
+}
+
+template<bool inv>
+vector<cmpl> fft(vector<cmpl>& in) {
+	vector<cmpl> out(sz(in));
+	fft<inv>(in.begin(), out.begin(), sz(in));
+	if (inv) each(x, out) x /= sz(in);
+	return out;
 }
