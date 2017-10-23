78e1def530f4933cb31a82b06726042cd40c283e
Teapot418 <teapot404@gmail.com>
2017-10-23 17:07:23 +0200

    Modular arithmetic

---
+++ lib/math/modular.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "../template.h"
+
+constexpr ll MOD = 234567899;
+
+// Compute modular inverse of a mod b
+ll modInv(ll a, ll m) { return (a == 1 ? 1 : ((a - modInv(m%a, a))*m + 1) / a); }
+
+// Integer mod p container
+struct Zp {
+	ll x;
+	Zp(ll y) : x(y % MOD) {}
+
+	Zp operator+(Zp r) const { return x+r.x; }
+	Zp operator-(Zp r) const { return x-r.x; }
+	Zp operator*(Zp r) const { return x*r.x; }
+	Zp operator/(Zp r) const { return x*modInv(r.x, MOD); }
+
+	Zp inv() { return modInv(x, MOD); }
+
+	Zp pow(ll e) {
+		if (!e) return 1;
+		Zp t = pow(e/2);
+		return t*t * (e % 2 ? x : 1);
+	}
+};
