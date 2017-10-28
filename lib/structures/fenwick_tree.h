a28389fab547e384926dbdce947ec62aa7ad80d4
Teapot418 <teapot404@gmail.com>
2017-10-28 12:12:45 +0200

    Fenwick tree: comments

--- lib/structures/fenwick_tree.h
+++ lib/structures/fenwick_tree.h
@@ -2,19 +2,21 @@
 #include "../template.h"
 
 // Binary Indexed Tree (Fenwick tree) [UNTESTED]
-// Time complexity: query O(lg n), add O(lg n)
+// Time complexity:
+// - modify point:         O(lg n)
+// - query interval [0;i): O(lg n)
 
 template<class T> struct Fenwick {
 	vector<T> s;
-	Fenwick(int n) : s(n) {}
+	Fenwick(int n) : s(n) {} // Change here identity
 
-	void add(int i, T v) {
-		for (; i < sz(s); i |= i+1) s[i] += v;
+	void modify(int i, T v) {
+		for (; i < sz(s); i |= i+1) s[i] += v; // Change here operation
 	}
 
-	T sum(int i) {
+	T query(int i) {
 		T v = 0;
-		for (; i > 0; i &= i-1) v += s[i];
+		for (; i > 0; i &= i-1) v += s[i]; // Change here operation
 		return v;
 	}
 };
