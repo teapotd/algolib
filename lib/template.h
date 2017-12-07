bc0c4568b6cb9f3d242b7b8a6fd283d5eb54670b
Teapot418 <teapot404@gmail.com>
2017-12-07 13:32:00 +0100

    Added cin.exceptions

--- lib/template.h
+++ lib/template.h
@@ -23,4 +23,4 @@ using Pii  = pair<int, int>;
 #define gcd      __gcd
 #define popcount __builtin_popcount
 
-// cin.sync_with_stdio(false); cin.tie(0);
+// cin.sync_with_stdio(false); cin.tie(0); cin.exceptions(cin.failbit);
