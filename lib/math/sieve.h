e6b1b9037945638ba83e956e4bef47fa1034a548
Teapot418 <teapot404@gmail.com>
2017-12-31 17:12:27 +0100

    math/sieve: Tests and fixes

--- lib/math/sieve.h
+++ lib/math/sieve.h
@@ -1,22 +1,15 @@
 #pragma once
 #include "../template.h"
-// !!IGNORE
 
-// Calculate smallest prime factors for all numbers in [0;MAX_P] [UNTESTED]
-// Time complexity: O(n lg lg n)
-
-constexpr int MAX_P = 1000000;
-
-Vi primes(MAX_P+1);
+constexpr int MAX_P = 1e6;
+Vi factor(MAX_P+1);
 
 void sieve() {
-	primes[0] = 0;
-	primes[1] = 1;
+	for (int i = 2; i*i <= MAX_P; i++)
+		if (!factor[i])
+			for (int j = i*i; j <= MAX_P; j += i)
+				if (!factor[j])
+					factor[j] = i;
 
-	for (int i = 2; i*i <= MAX_P; i++) {
-		if (primes[i]) continue;
-		for (int j = i*i; j <= MAX_P; j += i) if (!primes[j]) primes[j] = i;
-	}
-
-	rep(i, 2, MAX_P+1) if (!primes[i]) primes[i] = i;
+	rep(i,0,MAX_P+1) if (!factor[i]) factor[i]=i;
 }
--- tests/math/sieve.cpp
+++ tests/math/sieve.cpp
@@ -3,18 +3,16 @@
 
 int main() {
 	sieve();
+	assert(factor[0] == 0);
+	assert(factor[1] == 1);
 
 	for (int i = 2; i <= MAX_P; i++) {
 		int tmp = i, n = 1;
-		// printf("%d = ", i);
 
 		while (tmp > 1) {
-			n *= primes[tmp];
-			// printf("%d ", primes[tmp]);
-			tmp /= primes[tmp];
+			n *= factor[tmp];
+			tmp /= factor[tmp];
 		}
-		// printf("\n");
-
 		assert(i == n);
 	}
 	return 0;
