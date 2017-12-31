d979da30eb87d8d4f7cd5ef412e4d6ced4059b8e
Teapot418 <teapot404@gmail.com>
2017-12-31 03:15:30 +0100

    Style fix

--- lib/math/bellman_inequalities.h
+++ lib/math/bellman_inequalities.h
@@ -7,10 +7,8 @@ struct Constraint {
 
 bool solveIneq(vector<Constraint>& edges,
 	             vector<ll>& vars) {
-	rep(i, 0, sz(vars)) each(e, edges) {
-		vars[e.b] = min(vars[e.b],
-			              vars[e.a] - e.limit);
-	}
+	rep(i, 0, sz(vars)) each(e, edges)
+		vars[e.b]=min(vars[e.b],vars[e.a]-e.limit);
 	each(e, edges)
 		if (vars[e.a] - e.limit < vars[e.b])
 			return false;
