0d502d7ebe0a4e91b2cc69511455498ef735e541
teapotd <teapot404@gmail.com>
2018-07-18 20:04:39 +0200

    graphs/dense_graph

---
+++ tests/graphs/dense_graph.cpp
@@ -0,0 +1,29 @@
+#include "../../lib/graphs/dense_graph.h"
+#include "../testing.h"
+
+int n = 100000/64*64;
+uint64_t masks[128];
+DenseGraph G(n);
+
+void dfs(int i) {
+	G.visit(i);
+	G.dfsStep(i, dfs);
+}
+
+uint64_t randMask() {
+	uint64_t ret = -1;
+	rep(i, 0, 2) ret &= masks[rand()%128];
+	return ret;
+}
+
+int main() {
+	mt19937_64 twister;
+	each(x, masks) x = twister();
+
+	each(m, G.M) m = randMask();
+
+	auto before = now();
+	rep(i, 0, n) if (!G.isVisited(i)) dfs(i);
+	cout << since(before) << "ms" << endl;
+	return 0;
+}
