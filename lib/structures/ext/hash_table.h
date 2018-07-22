48191328cae26413a757bf90845c97ad02cf8b4a
teapotd <teapot404@gmail.com>
2018-07-22 08:17:51 +0200

    ext/hash_table: Anti-anti-hash

--- lib/structures/ext/hash_table.h
+++ lib/structures/ext/hash_table.h
@@ -4,3 +4,11 @@
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
 // gp_hash_table<K, V> = faster unordered_set
+
+// Anti-anti-hash
+const size_t HXOR = mt19937_64(time(0))();
+template<class T> struct SafeHash {
+	size_t operator()(const T& x) const {
+		return hash<T>()(x ^ T(HXOR));
+	}
+};
