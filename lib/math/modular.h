1501a4ec343cd37d7e59b9854bdc88a95fede38a
teapotd <teapot404@gmail.com>
2018-07-23 10:50:27 +0200

    math/modular: Faster Zp

--- lib/math/modular.h
+++ lib/math/modular.h
@@ -3,41 +3,49 @@
 
 constexpr ll MOD = 15*(1<<27)+1;
 
-ll modInv(ll a, ll m) {
-	if (a == 1) return 1;
-	return ((a - modInv(m%a, a))*m + 1) / a;
-}
-
-ll umod(ll a, ll m) {
-	a %= m; return (a >= 0 ? a : a+m);
-}
-
 struct Zp {
 	ll x;
-	Zp(ll y = 0) {
-		x = (y < 0 || y >= MOD ? umod(y, MOD) : y);
+	Zp(ll a = 0) { x=a%MOD; if (x<0) x+=MOD; }
+	static Zp wrap(ll a){Zp t; t.x=a; return t;}
+
+	Zp operator+(Zp r) const {
+		ll a = x+r.x;
+		return wrap(a >= MOD ? a-MOD : a);
 	}
 
-	Zp operator+(Zp r) const { return x+r.x; }
-	Zp operator-(Zp r) const { return x-r.x; }
-	Zp operator*(Zp r) const { return x*r.x; }
+	Zp operator-(Zp r) const {
+		ll a = x-r.x;
+		return wrap(a < 0 ? a+MOD : a);
+	}
+
+	Zp operator*(Zp r) const {
+		return wrap(x*r.x % MOD);
+	}
+
 	Zp operator/(Zp r) const {
-		return x * modInv(r.x, MOD);
+		return wrap(x*r.inv().x % MOD);
 	}
 
-	Zp inv() const { return modInv(x, MOD); }
+	Zp inv() const { return pow(MOD-2); }
 
 	Zp pow(ll e) const {
-		if (!e) return 1;
-		Zp t = pow(e/2);
-		return t*t * (e & 1 ? x : 1);
+		Zp t = wrap(1), m = *this;
+		while (e) {
+			if (e & 1) t *= m;
+			e >>= 1;
+			m *= m;
+		}
+		return t;
 	}
-
-	void print(...) { cerr << x; }
 
 	Zp& operator+=(Zp r){ return *this=*this+r; }
 	Zp& operator-=(Zp r){ return *this=*this-r; }
 	Zp& operator*=(Zp r){ return *this=*this*r; }
 	Zp& operator/=(Zp r){ return *this=*this/r; }
-	Zp  operator-() const { return -x; }
+	Zp operator-() const { return wrap(MOD-x); }
 };
+
+ll modInv(ll a, ll m) {
+	if (a == 1) return 1;
+	return ((a - modInv(m%a, a))*m + 1) / a;
+}
