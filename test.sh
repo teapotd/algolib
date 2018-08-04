85b579648137b1c089a037593d0bc41a801f21d0
teapotd <teapot404@gmail.com>
2018-08-04 13:28:45 +0200

    Scripts: gcc-7 xd

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -e
 ROOT=`pwd`
 
 mkdir -p `dirname $ROOT/build/tests/$1`
-g++-8 -std=c++11 -O2 -g -Wall -Wextra -Wfatal-errors -Wshadow    \
+g++-7 -std=c++11 -O2 -g -Wall -Wextra -Wfatal-errors -Wshadow    \
       -Wconversion -Wfloat-equal -Wlogical-op -Wformat=2 -DLOC   \
       -o $ROOT/build/tests/$1 $ROOT/tests/$1.cpp
 time $ROOT/build/tests/$1
