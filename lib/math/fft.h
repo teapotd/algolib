840f413ce3d357a065739c2f4459f2f7df16a8a1
teapotd <teapot404@gmail.com>
2018-08-06 11:43:41 +0200

    math/fft: Docs

--- lib/math/fft.h
+++ lib/math/fft.h
@@ -2,7 +2,12 @@
 #include "../template.h"
 #include "modular.h"
 
-// MOD = 15*(1<<27)+1
+// In-place Fast Fourier Transform
+// over Z_p or complex; time: O(n lg n)
+// DFT is in bit-reversed order!
+// Default uncommented version is Z_p
+
+// MOD = 15*(1<<27)+1          // Set this MOD!
 constexpr ll ROOT = 440564289; // order = 1<<27
 
 // using Vfft = vector<complex<double>>;
@@ -11,14 +16,12 @@ Vfft bases;
 
 void initFFT(int n) { // n must be power of 2
 	bases.resize(n+1, 1);
-
 //auto b = exp(complex<double>(0, 2*M_PI/n));
 	auto b = Zp(ROOT).pow((1<<27) / n);
-
 	rep(i, 1, n+1) bases[i] = b * bases[i-1];
 }
 
-template<int dir>
+template<int dir> // 1 for DFT, -1 for inverse
 void fft(Vfft& buf) {
 	int n = sz(buf), bits = 31-__builtin_clz(n);
 	int i = (dir > 0 ? 0 : bits-1);
@@ -45,6 +48,8 @@ void fft(Vfft& buf) {
 	if (dir < 0) each(x, buf) x = x/n;
 }
 
+// Compute convolution of a and b; O(n lg n)
+// Set a and b size appropriately
 Vfft convolve(Vfft a, Vfft b) {
 	fft<1>(a); fft<1>(b);
 	rep(i, 0, sz(a)) a[i] = a[i]*b[i];
