6de48af7e182d9c7e4dbfa414cc425ccb10c7e61
teapotd <teapot404@gmail.com>
2018-08-10 13:22:51 +0200

    geometry/convex_hull

---
+++ lib/geometry/convex_hull.cpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "../template.h"
+#include "vec2.h"
+
+// Find convex hull of points; time: O(n)
+// Points are returned counter-clockwise.
+// UNTESTED
+vector<vec2> convexHull(vector<vec2> points) {
+	auto pivot = mp(points[0].y, points[0].x);
+	each(p,points) pivot=min(pivot, mp(p.y,p.x));
+	each(p,points) p = p-vec2(pivot.y, pivot.x);
+
+	sort(all(points));
+	vector<vec2> hull;
+
+	each(p, points) {
+		while (sz(hull) >= 2) {
+			vec2 prev = hull.back()-hull[sz(hull)-2];
+			vec2 cur = p - hull.back();
+			if (prev.cross(cur) > 0) break;
+			hull.pop_back();
+		}
+		hull.pb(p);
+	}
+	return hull;
+}
