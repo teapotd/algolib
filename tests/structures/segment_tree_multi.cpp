134b52950edf911076d82bace15c4ecbc6228499
teapotd <teapot404@gmail.com>
2018-08-10 13:00:21 +0200

    Unfinished experimental multidimensional segment tree

---
+++ tests/structures/segment_tree_multi.cpp
@@ -0,0 +1,9 @@
+#include "../../lib/structures/segment_tree_multi.h"
+#include "../testing.h"
+
+int main() {
+	SegmentTreeMulti tree;
+	tree.init({ 10, 10, 10 });
+	tree.query(0, 2, 0, 2, 0, 2);
+	return 0;
+}
