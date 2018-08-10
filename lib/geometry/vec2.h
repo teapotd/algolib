8ce5219913cfc3f3d48b6bcde2dbfcad65c260f5
teapotd <teapot404@gmail.com>
2018-08-10 13:51:46 +0200

    geometry/vec2: Separate version for int and double

--- lib/geometry/vec2.h
+++ lib/geometry/vec2.h
@@ -3,63 +3,68 @@
 #include "../util/float.h" // Float comparison
 
 // 2D point / vector structure; UNTESTED
-struct vec2 {
-	using T = double;
-	T x{0}, y{0};
 
-	vec2() {}
-	vec2(T a, T b) : x(a), y(b) {}
+// Base class of versions for ints and doubles
+template<class T, class S> struct bvec2 {
+	T x, y;
+	S operator+(S r) const {return{x+r.x,y+r.y};}
+	S operator-(S r) const {return{x-r.x,y-r.y};}
+	S operator*(T r) const { return {x*r, y*r}; }
+	S operator/(T r) const { return {x/r, y/r}; }
 
-	vec2 operator+(vec2 r) const {
-		return {x+r.x, y+r.y};
-	}
-	vec2 operator-(vec2 r) const {
-		return {x-r.x, y-r.y};
-	}
-	vec2 operator*(T r) const {
-		return {x*r, y*r};
-	}
-	vec2 operator/(T r) const {
-		return {x/r, y/r};
-	}
-
-	T dot(vec2 r) const { return x*r.x+y*r.y; }
-	T cross(vec2 r) const { return x*r.y-y*r.x; }
-	T len2() const { return x*x + y*y; }
+	T dot(S r)   const { return x*r.x+y*r.y; }
+	T cross(S r) const { return x*r.y-y*r.x; }
+	T len2()     const { return x*x + y*y; }
 	double len() const { return sqrt(len2()); }
-	vec2 unit() const { return *this/len(); }
-
-	// Rotations are counterclockwise
-	vec2 perp() const { return {-y,x}; } // 90deg
-	vec2 rotate(double a) const {
-		return {x*cos(a) - y*sin(a),
-			      x*sin(a) + y*cos(a)};
-	}
+	S perp()     const { return {-y,x}; } //90deg
 
 	double angle() const { //[0;2*PI] CCW from OX
 		double a = atan2(y, x);
 		return (a < 0 ? a+2*M_PI : a);
 	}
+};
 
-	bool operator==(vec2 r) const {
-		// For floats:
-		return !cmp(x, r.x) && !cmp(y, r.y);
-		// For integers:
-		// return x == r.x && y == r.y;
+// Version for integer coordinates (long long)
+struct vec2i : bvec2<ll, vec2i> {
+	vec2i() : bvec2{0, 0} {}
+	vec2i(ll a, ll b) : bvec2{a, b} {}
+
+	bool operator==(vec2i r) const {
+		return x == r.x && y == r.y;
 	}
 
 	// Sort by angle, length if angles equal
-	bool operator<(vec2 r) const {
-		// For doubles:
-		int t = cmp(angle(), r.angle());
-		return t < 0 || (!t && len2()<r.len2());
-		// For integers:
-		// if (upper()!=r.upper()) return upper();
-		// auto t = cross(r);
-		// return t>0 || (!t && len2()<r.len2());
+	bool operator<(vec2i r) const {
+		if (upper() != r.upper()) return upper();
+		auto t = cross(r);
+		return t > 0 || (!t && len2() < r.len2());
 	}
 
-	// bool upper() const { // For ints
-	// 	return y > 0 || (y == 0 && x >= 0);
-	// }
+	bool upper() const {
+		return y > 0 || (y == 0 && x >= 0);
+	}
 };
+
+// Version for double coordinates
+struct vec2d : bvec2<double, vec2d> {
+	vec2d() : bvec2{0, 0} {}
+	vec2d(double a, double b) : bvec2{a, b} {}
+
+	vec2d unit() const { return *this/len(); }
+	vec2d rotate(double a) const { // CCW
+		return {x*cos(a) - y*sin(a),
+			      x*sin(a) + y*cos(a)};
+	}
+
+	bool operator==(vec2d r) const {
+		return !cmp(x, r.x) && !cmp(y, r.y);
+	}
+
+	// Sort by angle, length if angles equal
+	bool operator<(vec2d r) const {
+		int t = cmp(angle(), r.angle());
+		return t < 0 || (!t && len2()<r.len2());
+	}
+};
+
+using vec2 = vec2d;
