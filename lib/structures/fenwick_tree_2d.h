56dd1d569f0c83cc9e3e24f7f8d34bb522fd2689
teapotd <teapot404@gmail.com>
2018-08-13 17:46:43 +0200

    structures/fenwick_tree_2d

---
+++ lib/structures/fenwick_tree_2d.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "../template.h"
+
+// Fenwick tree 2D (BIT tree 2D); space: O(n*m)
+// Default version: prefix sums 2D
+// Change s to hashmap for O(q lg^2 n) memory
+struct Fenwick2D {
+	using T = int;
+	static constexpr T ID = 0;
+	T f(T a, T b) { return a+b; }
+
+	vector<T> s;
+	int w, h;
+
+	Fenwick2D(int n=0, int m=0) { init(n, m); }
+	void init(int n, int m) {
+		s.assign(n*m, ID); w = n; h = m;
+	}
+
+	// A[i,j] = f(A[i,j], v); time: O(lg^2 n)
+	void modify(int i, int j, T v) {
+		for (; i < w; i |= i+1)
+			for (int k = j; k < h; k |= k+1) {
+				T& x = s[i*h+k]; x = f(x, v);
+			}
+	}
+
+	// Query prefix; time: O(lg^2 n)
+	T query(int i, int j) {
+		T v = ID;
+		for (; i>0; i&=i-1)
+			for (int k = j; k > 0; k &= k-1)
+				v = f(v, s[i*h+k-h-1]);
+		return v;
+	}
+};
