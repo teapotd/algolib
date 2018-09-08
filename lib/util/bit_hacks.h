aa90922d58721d9097da447dda8042d6cf0c815e
teapotd <teapot404@gmail.com>
2018-09-08 15:14:39 +0200

    util/bit_hacks: Simplified transpose64

--- lib/util/bit_hacks.h
+++ lib/util/bit_hacks.h
@@ -9,21 +9,21 @@
 
 using ull = uint64_t;
 
-#define T64(s,up,down)               \
-	for (ull i=0; i<64; i+=s*2)        \
-		for (ull j = i; j < i+s; j++) {  \
-			ull a = (M[j] >> s) & up;      \
-			ull b = (M[j+s] << s) & down;  \
-			M[j] = (M[j] & up) | b;        \
-			M[j+s] = (M[j+s] & down) | a;  \
+#define T64(s,up)                      \
+	for (ull i=0; i<64; i+=s*2)          \
+		for (ull j = i; j < i+s; j++) {    \
+			ull a = (M[j] >> s) & up;        \
+			ull b = (M[j+s] & up) << s;      \
+			M[j] = (M[j] & up) | b;          \
+			M[j+s] = (M[j+s] & (up<<s)) | a; \
 		}
 
 // Transpose 64x64 bit matrix
 void transpose64(array<ull, 64>& M) {
-	T64(1,0x5555555555555555,0xAAAAAAAAAAAAAAAA);
-	T64(2,0x3333333333333333,0xCCCCCCCCCCCCCCCC);
-	T64(4,0xF0F0F0F0F0F0F0F,0xF0F0F0F0F0F0F0F0);
-	T64(8,0xFF00FF00FF00FF,0xFF00FF00FF00FF00);
-	T64(16,0xFFFF0000FFFF,0xFFFF0000FFFF0000);
-	T64(32,0xFFFFFFFF,0xFFFFFFFF00000000);
+	T64(1,  0x5555555555555555);
+	T64(2,  0x3333333333333333);
+	T64(4,  0xF0F0F0F0F0F0F0F);
+	T64(8,  0xFF00FF00FF00FF);
+	T64(16, 0xFFFF0000FFFF);
+	T64(32, 0xFFFFFFFFLL);
 }
