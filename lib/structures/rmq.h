9a51f0ccaea04d1c818e2ffa22b339c77faeef1f
teapotd <teapot404@gmail.com>
2018-10-15 17:02:54 +0200

    structures/rmq

---
+++ lib/structures/rmq.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "../template.h"
+
+// Range Minimum Query; space: O(n lg n)
+struct RMQ {
+	using T = int;
+	static constexpr T ID = INT_MAX;
+	T f(T a, T b)  { return min(a, b); }
+
+	vector<vector<T>> s;
+
+	RMQ() {}
+	RMQ(const vector<T>& vec) { init(vec); }
+
+	// Initialize RMQ structure; time: O(n lg n)
+	void init(const vector<T>& vec) {
+		s = {vec};
+		for (int h = 1; h <= sz(vec); h *= 2) {
+			s.emplace_back();
+			auto& prev = s[sz(s)-2];
+			rep(i, 0, sz(vec)-h*2+1)
+				s.back().pb(f(prev[i], prev[i+h]));
+		}
+	}
+
+	// Query f(s[b], ... ,s[e-1]); time: O(1)
+	T query(int b, int e) {
+		if (b >= e) return ID;
+		int k = 31 - __builtin_clz(e-b);
+		return f(s[k][b], s[k][e - (1<<k)]);
+	}
+};
---
+++ tests/structures/rmq.cpp
@@ -0,0 +1,26 @@
+#include "../../lib/structures/rmq.h"
+#include "../testing.h"
+
+void run() {
+	int n = r(1, 500);
+
+	Vi elems(n);
+	each(x, elems) x = r(-1000, 1000);
+
+	RMQ rmq(elems);
+
+	rep(b, 0, n+1) {
+		int id = RMQ::ID;
+
+		rep(e, 0, n+1) {
+			assert(id == rmq.query(b, e));
+			if (e >= b) id = min(id, elems[e]);
+		}
+	}
+}
+
+int main() {
+	rep(i, 0, 100) {
+		run();
+	}
+}
