f4e5a17f0bbd17393a6d0aa6c2d370308ed3bfbe
teapotd <teapot404@gmail.com>
2018-10-21 15:29:56 +0200

    Fix KMP

--- lib/text/kmp.h
+++ lib/text/kmp.h
@@ -1,35 +1,28 @@
 #pragma once
 #include "../template.h"
 
-// KMP algorithm; helper function
-// Use functions below.
-template<class T>
-void kmp(const T& cur, const T& prev, Vi& ps) {
-	each(x, cur) {
-		int k = ps.back();
-		while (k >= 0 && prev[k] != x) k = ps[k];
-		ps.pb(k+1);
-	}
-}
-
 // Computes prefsuf array; time: O(n)
 // ps[i] = max prefsuf of [0;i); ps[0] := -1
-template<class T> Vi prefSuf(const T& str) {
+template<class T> Vi kmp(const T& str) {
 	Vi ps; ps.pb(-1);
-	kmp(str, str, ps);
+	each(x, str) {
+		int k = ps.back();
+		while (k >= 0 && str[k] != x) k = ps[k];
+		ps.pb(k+1);
+	}
 	return ps;
 }
 
 // Finds occurences of pat in vec; time: O(n)
 // Returns starting indices of matches.
-// `neutral` is separator to be used for concat
 template<class T>
-Vi match(const T& str, const T& pat) {
-	Vi ret, ps; ps.pb(-1);
-	kmp(pat, pat, ps);
-	kmp(str, pat, ps);
-	rep(i, sz(ps)-sz(str), sz(ps)) {
-		if (ps[i]==sz(pat)) ret.pb(i-2*sz(pat)-1);
+Vi match(const T& str, T pat) {
+	int n = sz(pat);
+	pat.pb(-1); // SET TO SOME UNUSED CHARACTER
+	pat.insert(pat.end(), all(str));
+	Vi ret, ps = kmp(pat);
+	rep(i, 0, sz(ps)) {
+		if (ps[i] == n) ret.pb(i-2*sz(pat)-2);
 	}
 	return ret;
 }
