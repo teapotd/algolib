ecb9bec2eac2c60aa2eca30997a0dc6aba47bfc6
teapotd <teapot404@gmail.com>
2018-10-31 11:07:25 +0100

    util/inversion_vector: countInversions

--- lib/util/inversion_vector.h
+++ lib/util/inversion_vector.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "../template.h"
 
-// Get inversion vector for permutation of
+// Get inversion vector for sequence of
 // numbers in [0;n); ret[i] = count of numbers
 // smaller than perm[i] to the left; O(n lg n)
 Vi encodeInversions(Vi perm) {
@@ -15,6 +15,25 @@ Vi encodeInversions(Vi perm) {
 		rep(i, 0, sz(perm)) {
 			if (perm[i] % 2) odd[perm[i]]++;
 			else ret[i] += odd[perm[i]+1];
+			cont += perm[i] /= 2;
+		}
+	}
+	return ret;
+}
+
+// Count inversions in sequence of numbers
+// in [0;n); time: O(n lg n)
+int countInversions(Vi perm) {
+	int ret = 0, cont = 1;
+	Vi odd;
+
+	while (cont) {
+		odd.assign(sz(perm)+1, 0);
+		cont = 0;
+
+		rep(i, 0, sz(perm)) {
+			if (perm[i] % 2) odd[perm[i]]++;
+			else ret += odd[perm[i]+1];
 			cont += perm[i] /= 2;
 		}
 	}
