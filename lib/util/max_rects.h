e2bd91c70018a8c05f53cd2002db3a020577e6c9
teapotd <teapot404@gmail.com>
2018-11-29 12:17:35 +0100

    util/max_rects

---
+++ lib/util/max_rects.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "../template.h"
+
+struct MaxRect {
+	// begin = first column of rectangle
+	// end = first column after rectangle
+	// hei = height of rectangle
+	// touch = columns of height hei inside
+	int begin, end, hei;
+	Vi touch; // sorted increasing
+};
+
+// Given consecutive column heights find
+// all inclusion-wise maximal rectangles
+// contained in "drawing" of columns; time O(n)
+vector<MaxRect> getMaxRects(Vi hei) {
+	hei.insert(hei.begin(), -1);
+	hei.pb(-1);
+	Vi reach(sz(hei), sz(hei)-1);
+	vector<MaxRect> ans;
+
+	for (int i = sz(hei)-1; --i;) {
+		int j = i+1, k = i;
+		while (hei[j] > hei[i]) j = reach[j];
+		reach[i] = j;
+
+		while (hei[k] > hei[i-1]) {
+			ans.pb({ i-1, 0, hei[k], {} });
+			auto& rect = ans.back();
+
+			while (hei[k] == rect.hei) {
+				rect.touch.pb(k-1);
+				k = reach[k];
+			}
+			rect.end = k-1;
+		}
+	}
+	return ans;
+}
---
+++ tests/util/max_rects.cpp
@@ -0,0 +1,7 @@
+#include "../../lib/util/max_rects.h"
+#include "../../lib/template.h"
+#include "../testing.h"
+
+int main() {
+	return 0;
+}
