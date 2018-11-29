06129e67d5f33afe983c1dba50c0c3f81d6d34c3
teapotd <teapot404@gmail.com>
2018-11-29 13:03:42 +0100

    ext/tree: Docs

--- lib/structures/ext/tree.h
+++ lib/structures/ext/tree.h
@@ -10,3 +10,8 @@ using ordered_set = tree<
 	T, null_type, Cmp, rb_tree_tag,
 	tree_order_statistics_node_update
 >;
+
+// Standard set functions and:
+// t.order_of_key(key) - index of first >= key
+// t.find_by_order(i) - find i-th element
+// t1.join(t2) - assuming t1<>t2 merge t2 to t1
