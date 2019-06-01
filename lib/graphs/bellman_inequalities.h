86ef73fc94f8b048e01cc0d6ec57746817991e71
teapotd <teapot404@gmail.com>
2019-06-01 13:37:00 +0200

    graphs/bellman_inequalities: Cosmetic changes

--- lib/graphs/bellman_inequalities.h
+++ lib/graphs/bellman_inequalities.h
@@ -3,7 +3,6 @@
 
 struct Ineq {
 	ll a, b, c; // a - b >= c
-	DBP(a, b, c);
 };
 
 // Solve system of inequalities of form a-b>=c
@@ -12,7 +11,7 @@ bool solveIneq(vector<Ineq>& edges,
 	             vector<ll>& vars) {
 	rep(i, 0, sz(vars)) each(e, edges)
 		vars[e.b] = min(vars[e.b], vars[e.a]-e.c);
-	each(e, edges) if (vars[e.a]-e.c < vars[e.b])
-		return false;
-	return true;
+	each(e, edges)
+		if (vars[e.a]-e.c < vars[e.b]) return 0;
+	return 1;
 }
