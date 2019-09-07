9838d133bbcbba4c105de4ede819abc3d191d171
teapotd <teapot404@gmail.com>
2019-09-08 00:18:04 +0200

    graphs/turbo_matching: Improvements and vertex cover

--- lib/graphs/turbo_matching.h
+++ lib/graphs/turbo_matching.h
@@ -1,54 +1,58 @@
 #pragma once
 #include "../template.h"
 
-// Find matching in bipartite graph; time: ?
-struct Matching {
-	vector<Vi> G; // Both sides together
-	Vi match; // Matched vertices, -1 if none
+// Find maximum bipartite matching; time: ?
+// G must be bipartite graph!
+// Returns matching size (edge count).
+// match[v] = vert matched to v or -1
+int matching(vector<Vi>& G, Vi& match) {
 	vector<bool> seen;
+	int n = 0, k = 1;
+	match.assign(sz(G), -1);
 
-	// Initialize for n vertices
-	Matching(int n = 0) { init(n); }
-	void init(int n) { G.assign(n, {}); }
-
-	// Add new vertex
-	int addVert() {
-		G.emplace_back();
-		return sz(G)-1;
-	}
-
-	// Add edge between u and v
-	void addEdge(int u, int v) {
-		G[u].pb(v); G[v].pb(u);
-	}
-
-	bool dfs(int i) {
+	function<int(int)> dfs = [&](int i) {
 		if (seen[i]) return 0;
 		seen[i] = 1;
-
 		each(e, G[i]) {
 			if (match[e] < 0 || dfs(match[e])) {
-				match[i] = e;
-				match[e] = i;
+				match[i] = e; match[e] = i;
 				return 1;
 			}
 		}
 		return 0;
-	}
+	};
 
-	// Find maximum bipartite matching.
-	// Returns matching size (edge count)
-	int solve() {
-		int n = 0, k = 1;
-		match.assign(sz(G), -1);
-
-		while (k) {
-			seen.assign(sz(G), 0);
-			k = 0;
-			rep(i, 0, sz(G)) if (match[i] < 0)
-				k += dfs(i);
-			n += k;
-		}
-		return n;
+	while (k) {
+		seen.assign(sz(G), 0);
+		k = 0;
+		rep(i, 0, sz(G)) if (match[i] < 0)
+			k += dfs(i);
+		n += k;
 	}
-};
+	return n;
+}
+
+// Convert maximum matching to vertex cover
+// time: O(n+m)
+Vi vertexCover(vector<Vi>& G, Vi& match) {
+	Vi ret, col(sz(G)), seen(sz(G));
+
+	function<void(int, int)> dfs =
+			[&](int i, int c) {
+		if (col[i]) return;
+		col[i] = c+1;
+		each(e, G[i]) dfs(e, !c);
+	};
+
+	function<void(int)> aug = [&](int i) {
+		if (seen[i] || col[i] != 1) return;
+		seen[i] = 1;
+		each(e, G[i]) seen[e] = 1, aug(match[e]);
+	};
+
+	rep(i, 0, sz(G)) dfs(i, 0);
+	rep(i, 0, sz(G)) if (match[i] < 0) aug(i);
+	rep(i, 0, sz(G))
+		if (seen[i] == col[i]-1) ret.pb(i);
+	return ret;
+}
