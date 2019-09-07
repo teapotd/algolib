95c42281aa9d820c67b32d1b035a6c48358be478
teapotd <teapot404@gmail.com>
2019-09-07 12:36:35 +0200

    modinv_precompute: compute up to vector size

--- lib/math/modinv_precompute.h
+++ lib/math/modinv_precompute.h
@@ -2,11 +2,11 @@
 #include "../template.h"
 
 constexpr ll MOD = 234567899;
-vector<ll> modInv(MOD);
+vector<ll> modInv(MOD); // You can lower size
 
 // Precompute modular inverses; time: O(MOD)
 void initModInv() {
 	modInv[1] = 1;
-	rep(i, 2, MOD) modInv[i] =
+	rep(i, 2, sz(modInv)) modInv[i] =
 		(MOD - (MOD/i) * modInv[MOD%i]) % MOD;
 }
