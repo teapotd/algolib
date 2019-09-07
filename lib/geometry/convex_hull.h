fc353c1830a58f0900727eb53c1c40b5c5480cb4
teapotd <teapot404@gmail.com>
2019-09-07 19:22:11 +0200

    geometry/convex_hull: Minor changes

--- lib/geometry/convex_hull.h
+++ lib/geometry/convex_hull.h
@@ -5,9 +5,9 @@
 // Translate points such that lower-left point
 // is (0, 0). Returns old point location; O(n)
 vec2 normPos(vector<vec2>& points) {
-	auto pivot = points[0].yxPair();
-	each(p,points) pivot = min(pivot,p.yxPair());
-	vec2 ret{pivot.y, pivot.x};
+	auto q = points[0].yxPair();
+	each(p, points) q = min(q, p.yxPair());
+	vec2 ret{q.y, q.x};
 	each(p, points) p = p-ret;
 	return ret;
 }
@@ -21,9 +21,9 @@ vector<vec2> convexHull(vector<vec2> points) {
 
 	each(p, points) {
 		while (sz(hull) >= 2) {
-			vec2 prev = hull.back()-hull[sz(hull)-2];
-			vec2 cur = p - hull.back();
-			if (prev.cross(cur) > 0) break;
+			vec2 a = hull.back() - hull[sz(hull)-2];
+			vec2 b = p - hull.back();
+			if (a.cross(b) > 0) break;
 			hull.pop_back();
 		}
 		hull.pb(p);
