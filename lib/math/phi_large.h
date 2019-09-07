6daa34a5d6fa18f649128283141564b905423d9c
teapotd <teapot404@gmail.com>
2019-09-07 22:46:22 +0200

    math/phi_large: Update docs

--- lib/math/phi_large.h
+++ lib/math/phi_large.h
@@ -3,8 +3,7 @@
 #include "pollard_rho.h"
 
 // Compute Euler's totient of large numbers
-// time: O(n^(1/3)) <- factorization
-// You need to initialize Pollard's rho first!
+// time: O(n^(1/4)) <- factorization
 ll phi(ll n) {
 	each(p, factorize(n)) n = n / p.x * (p.x-1);
 	return n;
