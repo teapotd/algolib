868ad1a2d060591b99925d251444d6f597cd4173
teapotd <teapot404@gmail.com>
2019-09-07 19:05:27 +0200

    structures/max_queue: Improvements

--- lib/structures/max_queue.h
+++ lib/structures/max_queue.h
@@ -3,22 +3,22 @@
 
 // Queue with max query on contained elements
 struct MaxQueue {
-	using T = ll;
-	deque<T> Q;
+	using T = int;
+	deque<T> Q, M;
 
 	// Add v to the back; time: amortized O(1)
 	void push(T v) {
-		while (!Q.empty() && Q.front() < v)
-			Q.pop_front();
-		Q.push_front(v);
+		while (!M.empty() && M.back() < v)
+			M.pop_back();
+		M.pb(v); Q.pb(v);
 	}
 
-	// Pop from the back (v must be the last one)
-	// time: amortized O(1)
-	void pop(T v) {
-		if (Q.back() == v) Q.pop_back();
+	// Pop from the front; time: O(1)
+	void pop() {
+		if (M.front() == Q.front()) M.pop_front();
+		Q.pop_front();
 	}
 
 	// Get max element value; time: O(1)
-	T max() const { return Q.back(); }
+	T max() const { return M.front(); }
 };
