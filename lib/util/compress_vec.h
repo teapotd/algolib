46dfeed9c4f9884d560320721e88ca90ee08a581
teapotd <teapot404@gmail.com>
2019-09-07 15:57:20 +0200

    util/compress_vec: Save original values

--- lib/util/compress_vec.h
+++ lib/util/compress_vec.h
@@ -3,15 +3,15 @@
 
 // Compress integers to range [0;n) while
 // preserving their order; time: O(n lg n)
-// Returns count n of different numbers
-int compressVec(vector<int*>& vec) {
-	sort(all(vec), [](int* l, int* r) {
-		return *l < *r;
-	});
-	int last = *vec[0], i = 0;
+// Returns mapping: compressed -> original
+Vi compressVec(vector<int*>& vec) {
+	sort(all(vec),
+		[](int* l, int* r) { return *l < *r; });
+	Vi old;
 	each(e, vec) {
-		if (*e != last) i++;
-		last = *e; *e = i;
+		if (old.empty() || old.back() != *e)
+			old.pb(*e);
+		*e = sz(old)-1;
 	}
-	return i+1;
+	return old;
 }
