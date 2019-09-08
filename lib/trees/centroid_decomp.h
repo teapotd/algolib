5789092c43ba0c2672c5239bdaddd3680d6b85e5
teapotd <teapot404@gmail.com>
2019-09-08 17:47:17 +0200

    tress/centroid_decomp: Refactoring

--- lib/trees/centroid_decomp.h
+++ lib/trees/centroid_decomp.h
@@ -3,77 +3,69 @@
 
 // Centroid decomposition; space: O(n lg n)
 // UNTESTED
-
-struct Vert {
-	// cE = edges to children centroids
-	// cLinks[i] = vertex index in
+struct CentroidTree {
+	// child[v] = children of v in centroid tree
+	// ind[v][i] = index of vertex v in
 	//             i-th centroid from root
-	// cSubtree = vertices in centroid subtree
-	// cDists = distances to vertices in subtree
-	// cParent = parent centroid
-	// cDepth = depth in centroid tree
-	// cSize = subtree size
-	Vi E, cE, cLinks, cSubtree, cDists;
-	int cParent{-2}, cDepth{-1}, cSize{-1};
-};
+	// subtree[v] = vertices in centroid subtree
+	// dists[v] = distances from v to vertices
+	//            in centroid subtree
+	// par[v] = parent of v in centroid tree
+	// depth[v] = depth of v in centroid tree
+	// size[v] = size of centroid subtree of v
+	vector<Vi> child, ind, dists, subtree;
+	Vi par, depth, size;
+	int root; // Root centroid
 
-vector<Vert> G;
+	CentroidTree() {}
 
-// Check if DFS should go to edge e if it came
-// to current node from p. Also ignores
-// vertices with cParent != -2 (established
-// centroids). Used by functions below
-bool can(int e, int p) {
-	return e != p && G[e].cParent == -2;
-}
+	CentroidTree(vector<Vi>& G)
+			: child(sz(G)), ind(sz(G)), dists(sz(G)),
+			  subtree(sz(G)), par(sz(G), -2),
+			  depth(sz(G)), size(sz(G)) {
+		root = decomp(G, 0, 0);
+	}
 
-// Computes subtree sizes
-int computeSize(int i, int p) {
-	int& s = G[i].cSize = 1;
-	each(e, G[i].E) if (can(e, p))
-		s += computeSize(e, i);
-	return s;
-}
+	int dfs(vector<Vi>& G, int i, int p) {
+		size[i] = 1;
+		each(e, G[i]) if (e != p && par[e] == -2)
+			size[i] += dfs(G, e, i);
+		return size[i];
+	}
 
-// Finds centroid
-int getCentroid(int i) {
-	int p = -1, size = computeSize(i, -1);
-	bool ok = true;
-	while (ok) {
-		ok = false;
-		each(e, G[i].E) if (can(e, p)) {
-			if (G[e].cSize > size/2) {
-				p = i; i = e; ok = true;
-				break;
+	void layer(vector<Vi>& G, int i,
+	           int p, int c, int d) {
+		ind[i].pb(sz(subtree[c]));
+		subtree[c].pb(i);
+		dists[c].pb(d);
+		each(e, G[i]) if (e != p && par[e] == -2)
+			layer(G, e, i, c, d+1);
+	}
+
+	int decomp(vector<Vi>& G, int v, int d) {
+		int p = -1, s = dfs(G, v, -1);
+		bool ok = 1;
+		while (ok) {
+			ok = 0;
+			each(e, G[v]) {
+				if (e != p && par[e] == -2 &&
+						size[e] > s/2) {
+					p = v; v = e; ok = 1;
+					break;
+				}
 			}
 		}
+
+		par[v] = -1;
+		size[v] = s;
+		depth[v] = d;
+		layer(G, v, -1, v, 0);
+
+		each(e, G[v]) if (par[e] == -2) {
+			int j = decomp(G, e, d+1);
+			child[v].pb(j);
+			par[j] = v;
+		}
+		return v;
 	}
-	G[i].cSize = size;
-	return i;
-}
-
-// Calculate centroid subtree data
-void dfsLayer(int i, int p, int centr, int d) {
-	G[i].cLinks.pb(sz(G[centr].cSubtree));
-	G[centr].cSubtree.pb(i);
-	G[centr].cDists.pb(d);
-
-	each(e, G[i].E) if (can(e, p))
-		dfsLayer(e, i, centr, d+1);
-}
-
-// Perform centroid decomposition of tree
-// containing `i`; time: O(n lg n)
-// Returns root centroid.
-int centroidDecomp(int i, int depth = 0) {
-	i = getCentroid(i);
-	G[i].cParent = -1;
-	G[i].cDepth = depth;
-	dfsLayer(i, -1, i, 0);
-
-	each(e, G[i].E) if (can(e, -1)) {
-		G[i].cE.pb(centroidDecomp(e, depth+1));
-		G[G[i].cE.back()].cParent = i;
-	}
-	return i;
-}
+};
--- tests/trees/centroid_decomp.cpp
+++ tests/trees/centroid_decomp.cpp
@@ -1,9 +1,12 @@
 #include "../../lib/trees/centroid_decomp.h"
 #include "../testing.h"
 
+vector<Vi> G;
+CentroidTree cen;
+
 void edge(int i, int j) {
-	G[i-1].E.pb(j-1);
-	G[j-1].E.pb(i-1);
+	G[i-1].pb(j-1);
+	G[j-1].pb(i-1);
 }
 
 void dfs(int v, int indent = 0) {
@@ -11,10 +14,10 @@ void dfs(int v, int indent = 0) {
 		printf(" ");
 	}
 
-	printf("%d (%d/%d): ", v+1, G[v].cDepth, G[v].cSize);
+	printf("%d (%d/%d): ", v+1, cen.depth[v], cen.size[v]);
 	printf("\n");
 
-	each(e, G[v].cE) dfs(e, indent+4);
+	each(e, cen.child[v]) dfs(e, indent+4);
 }
 
 int main() {
@@ -36,10 +39,11 @@ int main() {
 	edge(13, 15);
 	edge(13, 16);
 
-	dfs(centroidDecomp(0));
+	cen = {G};
+	dfs(cen.root);
 
 	rep(i, 0, sz(G)) {
-		deb(i, G[i].cLinks, G[i].cSubtree, G[i].cDists);
+		deb(i, cen.ind[i], cen.subtree[i], cen.dists[i]);
 	}
 	return 0;
 }
