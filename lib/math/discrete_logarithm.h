63e09b72bc2ebdcbcf9df956662f45fa59c90849
teapotd <teapot404@gmail.com>
2019-09-25 20:55:07 +0200

    math/discrete_logarithm

---
+++ lib/math/discrete_logarithm.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "../template.h"
+#include "../modular.h"
+
+// Baby-step giant-step algorithm; O(sqrt(p))
+// Finds smallest x such that a^x = b (mod p)
+// or returns -1 if there's no solution.
+ll dlog(ll a, ll b, ll p) {
+	int m = int(min(llround(sqrt(p))+1, p-1));
+	unordered_map<ll, int> small;
+	ll t = 1;
+
+	rep(i, 0, m) {
+		int& k = small[t];
+		if (!k) k = i+1;
+		t = t*a % p;
+	}
+
+	t = modInv(t, p);
+
+	rep(i, 0, m) {
+		int j = small[b];
+		if (j) return i*m + j - 1;
+		b = b*t % p;
+	}
+
+	return -1;
+}
