d719aa16e8feb70c11dc94253ba5b403635e9009
teapotd <teapot404@gmail.com>
2019-09-26 04:12:37 +0200

    text/manacher

---
+++ lib/text/manacher.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "../template.h"
+
+// Manacher algorithm; time: O(n)
+// Finds largest radiuses for palindromes:
+// r[2*i] = for center at i (single letter = 1)
+// r[2*i+1] = for center between i and i+1
+template<class T> Vi manacher(const T& str) {
+	int n = sz(str)*2, c = 0, e = 1;
+	Vi r(n, 1);
+	auto get = [&](int i) { return i%2 ? 0 :
+		(i >= 0 && i < n ? str[i/2] : i); };
+
+	rep(i, 0, n) {
+		if (i < e) r[i] = min(r[c*2-i], e-i);
+		while (get(i-r[i]) == get(i+r[i])) r[i]++;
+		if (i+r[i] > e) c = i, e = i+r[i]-1;
+	}
+
+	rep(i, 0, n) r[i] /= 2;
+	return r;
+}
---
+++ tests/text/manacher.cpp
@@ -0,0 +1,46 @@
+#include "../../lib/text/manacher.h"
+#include "../testing.h"
+
+Vi naiveManacher(const string& tmp) {
+	string str;
+	each(c, tmp) {
+		str.pb(c);
+		str.pb('#');
+	}
+
+	Vi ret(sz(str));
+
+	rep(i, 0, sz(str)) {
+		int k = 1;
+		while (i+k < sz(str) && i-k >= 0 && str[i-k] == str[i+k]) {
+			k++;
+		}
+
+		if (str[i-k+1] == '#') k--;
+		ret[i] = k/2 + (i%2 == 0);
+	}
+
+	return ret;
+}
+
+void test(int n, int alpha) {
+	string str;
+	rep(i, 0, n) {
+		str.pb(char('a' + r(0, alpha-1)));
+	}
+
+	Vi naive = naiveManacher(str);
+	Vi fast = manacher(str);
+	deb(naive, fast);
+	assert(naive == fast);
+}
+
+int main() {
+	rep(alpha, 1, 10) rep(n, 1, 20) {
+		deb(n, alpha);
+		rep(i, 0, 10) {
+			test(n, alpha);
+		}
+	}
+	return 0;
+}
