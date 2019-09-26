aba09235e7c25fbc068ccbfe4d677f8293909e09
teapotd <teapot404@gmail.com>
2019-09-26 02:45:13 +0200

    text/z_function

---
+++ lib/text/z_function.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "../template.h"
+
+// Computes Z function array; time: O(n)
+// zf[i] = max common prefix of str and str[i:]
+template<class T> Vi prefPref(const T& str) {
+	int n = sz(str), b = 0, e = 1;
+	Vi zf(n);
+	rep(i, 1, n) {
+		if (i < e) zf[i] = min(zf[i-b], e-i);
+		while (i+zf[i] < n &&
+			str[zf[i]] == str[i+zf[i]]) zf[i]++;
+		if (i+zf[i] > e) b = i, e = i+zf[i];
+	}
+	zf[0] = n;
+	return zf;
+}
