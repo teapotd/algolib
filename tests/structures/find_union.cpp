025d4feb7f83d2058f6ef094c46ca685fcef3449
teapotd <teapot404@gmail.com>
2019-11-05 12:49:48 +0100

    structures/find_union: Benchmark

--- tests/structures/find_union.cpp
+++ tests/structures/find_union.cpp
@@ -1,7 +1,22 @@
 #include "../../lib/structures/find_union.h"
 #include "../testing.h"
 
+void bench() {
+	rep(t, 0, 5) {
+		int n = r(5e6, 5e6+500);
+		FAU fau(n);
+		rep(i, 0, 5e6) {
+			int a = r(0, n-1), b = r(0, n-1);
+			fau.join(a, b);
+		}
+		cout << fau.find(12345) << endl;
+	}
+}
+
 int main() {
+	// bench();
+	// return 0;
+
 	rep(total, 0, 10) {
 		int n = r(0, 1000);
 		FAU fau(n);
