2e88fc6e272b1c7df95ea25474695a10be0bed1d
teapotd <teapot404@gmail.com>
2019-11-08 13:01:29 +0100

    structures/treap: More docs

--- lib/structures/treap.h
+++ lib/structures/treap.h
@@ -2,27 +2,44 @@
 #include "../template.h"
 
 // "Set" of implicit keyed treaps; space: O(n)
-// Treaps are distinguished by roots indices
+// Nodes are keyed by their indices in array
+// of all nodes. Treap key is key of its root.
+// "Node x" means "node with key x".
+// "Treap x" means "treap with key x".
+// Key -1 is "null".
 // Put any additional data in Node struct.
 struct Treap {
 	struct Node {
+		// E[0] = left child, E[1] = right child
+		// weight = node random weight (for treap)
+		// size = subtree size, par = parent node
 		int E[2] = {-1, -1}, weight{rand()};
 		int size{1}, par{-1};
-		bool flip{false}; // Is interval reversed?
+		bool flip{0}; // Is interval reversed?
 	};
 
-	vector<Node> G;
+	vector<Node> G; // Array of all nodes
 
-	// Initialize structure for n nodes; O(n)
+	// Initialize structure for n nodes
+	// with keys 0, ..., n-1; time: O(n)
 	// Each node is separate treap,
-	// use join() to construct sequence.
+	// use join() to make sequence.
 	Treap(int n = 0) : G(n) {}
 
-	int size(int x) { // Returns subtree size
+	// Create new treap (a single node),
+	// returns its key; time: O(1)
+	int make() {
+		G.emplace_back(); return sz(G)-1;
+	}
+
+	// Get size of node x subtree. x can be -1.
+	int size(int x) { // time: O(1)
 		return (x >= 0 ? G[x].size : 0);
 	}
 
-	void push(int x) { // Propagates down stuff
+	// Propagate down data (flip flag etc).
+	// x can be -1; time: O(1)
+	void push(int x) {
 		if (x >= 0 && G[x].flip) {
 			G[x].flip = 0;
 			swap(G[x].E[0], G[x].E[1]);
@@ -30,7 +47,9 @@ struct Treap {
 		} // + any other lazy operations
 	}
 
-	void update(int x) { // Updates aggregates
+	// Update aggregates of node x.
+	// x can be -1; time: O(1)
+	void update(int x) {
 		if (x >= 0) {
 			int& s = G[x].size = 1;
 			G[x].par = -1;
@@ -41,8 +60,10 @@ struct Treap {
 		} // + any other aggregates
 	}
 
-	// Split treap x by index i into l and r
-	// average time: O(lg n)
+	// Split treap x into treaps l and r
+	// such that l contains first i elements
+	// and r the remaining ones.
+	// x, l, r can be -1; time: ~O(lg n)
 	void split(int x, int& l, int& r, int i) {
 		push(x); l = r = -1;
 		if (x < 0) return;
@@ -57,8 +78,11 @@ struct Treap {
 		update(x);
 	}
 
-	// Join two treaps in given order; O(lg n)
-	int join(int l, int r) {
+	// Join treaps l and r into one treap
+	// such that elements of l are before
+	// elements of r. Returns new treap.
+	// l, r and returned value can be -1.
+	int join(int l, int r) { // time: ~O(lg n)
 		push(l); push(r);
 		if (l < 0 || r < 0) return max(l, r);
 
@@ -73,7 +97,9 @@ struct Treap {
 		return r;
 	}
 
-	// Find node with index i in treap x; O(lg n)
+	// Find i-th node in treap x.
+	// Returns its key or -1 if not found.
+	// x can be -1; time: ~O(lg n)
 	int find(int x, int i) {
 		while (x >= 0) {
 			push(x);
@@ -82,10 +108,11 @@ struct Treap {
 			x = G[x].E[key < i];
 			if (key < i) i -= key+1;
 		}
-		return x;
+		return -1;
 	}
 
-	// Reverse interval [l;r) in treap x; O(lg n)
+	// Reverse interval [l;r) in treap x.
+	// Returns new key of treap; time: ~O(lg n)
 	int reverse(int x, int l, int r) {
 		int a, b, c;
 		split(x, b, c, r);
@@ -94,7 +121,9 @@ struct Treap {
 		return join(join(a, b), c);
 	}
 
-	// Find root of treap containing x; O(lg n)
+	// Get key of treap containing node x
+	// (key of treap root).
+	// x can be -1; time: ~O(lg n)
 	int root(int x) {
 		while (G[x].par >= 0) x = G[x].par;
 		return x;
