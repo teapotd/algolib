78c57d1f3482c9878726b0ff395a9e7dc21e0d76
teapotd <teapot404@gmail.com>
2019-11-23 16:16:01 +0100

    geometry/line2: Docs

--- src/geometry/line2.h
+++ src/geometry/line2.h
@@ -2,15 +2,20 @@
 #include "../template.h"
 #include "vec2.h"
 
-// 2D line structure; PARTIALLY TESTED
+// 2D line/halfplane structure
+// PARTIALLY TESTED
 
 // Base class of versions for ints and doubles
 template<class T, class P, class S>
-struct bline2 { // norm*point == off
+struct bline2 {
+	// For lines: norm*point == off
+	// For halfplanes: norm*point <= off
+	// (i.e. normal vector points outside)
 	P norm; // Normal vector [A; B]
 	T off;  // Offset (C parameter of equation)
 
-	// Line through 2 points
+	// Line through 2 points; normal vector
+	// points to the left of ab vector
 	static S through(P a, P b) {
 		return { (b-a).perp(), b.cross(a) };
 	}
@@ -36,6 +41,9 @@ struct line2i : bline2<ll, vec2i, line2i> {
 	line2i() : bline2{{}, 0} {}
 	line2i(vec2i n, ll c) : bline2{n, c} {}
 
+	// Returns 0 if point a lies on the line,
+	// 1 if on side where normal vector points,
+	// -1 if on the other side.
 	int side(vec2i a) {
 		ll d = norm.dot(a);
 		return (d > off) - (d < off);
@@ -48,10 +56,16 @@ struct line2d : bline2<double, vec2d, line2d> {
 	line2d() : bline2{{}, 0} {}
 	line2d(vec2d n, double c) : bline2{n, c} {}
 
+	// Returns 0 if point a lies on the line,
+	// 1 if on side where normal vector points,
+	// -1 if on the other side.
 	int side(vec2d a) {
 		return cmp(norm.dot(a), off);
 	}
 
+	// Intersect this line with line a, returns
+	// true on success (false if parallel).
+	// Intersection point is saved to `out`.
 	bool intersect(line2d a, vec2d& out) {
 		double d = norm.cross(a.norm);
 		if (cmp(d, 0) == 0) return false;
