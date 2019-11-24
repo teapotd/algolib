fe2200ad02a194f369f90dcbf857067c9b16db37
teapotd <teapot404@gmail.com>
2019-11-24 15:32:35 +0100

    math/fwht: Fixes and improvements

--- src/math/fwht.h
+++ src/math/fwht.h
@@ -4,9 +4,11 @@
 // Fast Walsh-Hadamard Transform; O(n lg n)
 // Input must be power of 2!
 // Uncommented version is for XOR.
-// UNTESTED
+// OR version is equivalent to sum-over-subsets
+// (Zeta transform, inverse is Moebius).
+// TESTED ON RANDS
 //! Source: https://github.com/kth-competitive-programming/kactl/blob/master/content/numerical/FastSubsetTransform.h
-template<class T, bool inv>
+template<bool inv, class T>
 void fwht(vector<T>& b) {
 	for (int s = 1; s < sz(b); s *= 2) {
 		for (int i = 0; i < sz(b); i += s*2) {
@@ -15,7 +17,7 @@ void fwht(vector<T>& b) {
 				tie(x, y) =
 				   mp(x+y, x-y);                  //XOR
 				// inv ? mp(y-x, x) : mp(y, x+y); //AND
-				// inv ? mp(y, x-y) : mp(x+y, y); //OR
+				// inv ? mp(x, y-x) : mp(x, x+y); //OR
 			}
 		}
 	}
@@ -24,13 +26,13 @@ void fwht(vector<T>& b) {
 	if (inv) each(e, b) e /= sz(b);
 }
 
-// BIT-convolve a and b, store result in a;
+// "Bit-convolve" a and b, store result in a;
 // time: O(n lg n)
 // Both arrays must be of same size = 2^n!
 template<class T>
 void convolve(vector<T>& a, vector<T> b) {
-	fwht<T, 0>(a);
-	fwht<T, 0>(b);
+	fwht<0>(a);
+	fwht<0>(b);
 	rep(i, 0, sz(a)) a[i] *= b[i];
-	fwht<T, 1>(a);
+	fwht<1>(a);
 }
--- tests/math/fwht.cpp
+++ tests/math/fwht.cpp
@@ -1,6 +1,46 @@
 #include "../../src/math/fwht.h"
 #include "../testing.h"
 
+vector<ll> naiveXorConv(vector<ll> A, vector<ll> B) {
+	vector<ll> ret(sz(A));
+	rep(i, 0, sz(A)) rep(j, 0, sz(A)) ret[i^j] += A[i]*B[j];
+	return ret;
+}
+
+vector<ll> naiveOrConv(vector<ll> A, vector<ll> B) {
+	vector<ll> ret(sz(A));
+	rep(i, 0, sz(A)) rep(j, 0, sz(A)) ret[i|j] += A[i]*B[j];
+	return ret;
+}
+
+vector<ll> naiveAndConv(vector<ll> A, vector<ll> B) {
+	vector<ll> ret(sz(A));
+	rep(i, 0, sz(A)) rep(j, 0, sz(A)) ret[i&j] += A[i]*B[j];
+	return ret;
+}
+
+template<class F, class G>
+void test(F fast, G naive) {
+	rep(i, 0, 100) {
+		int p = r(0, 10);
+		vector<ll> A(1<<p), B(1<<p);
+		each(k, A) k = r(-10000, 10000);
+		each(k, B) k = r(-10000, 10000);
+
+		vector<ll> expected = naive(A, B);
+		fast(A, B);
+
+		if (A != expected) {
+			deb(A);
+			deb(expected);
+			exit(0);
+		}
+	}
+}
+
 int main() {
+	test(convolve<ll>, naiveXorConv);
+	// test(convolve<ll>, naiveOrConv);
+	// test(convolve<ll>, naiveAndConv);
 	return 0;
 }
