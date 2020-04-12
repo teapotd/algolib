4ceca52ecbe8d46a09ff3c885c5ceab8f43578b0
teapotd <teapot404@gmail.com>
2020-04-12 13:52:36 +0200

    graphs/dense_dfs and math/bit_matrix: Tested in practice

--- src/graphs/dense_dfs.h
+++ src/graphs/dense_dfs.h
@@ -2,24 +2,33 @@
 #include "../template.h"
 #include "../math/bit_matrix.h"
 
-// DFS over adjacency matrix; time: O(n^2/64)
-// G = graph, V = not visited vertices masks
-// UNTESTED
+// DFS over bit-packed adjacency matrix
+// G = NxN adjacency matrix of graph
+//     G(i,j) <=> (i,j) is edge
+// V = 1xN matrix containing unvisited vertices
+//     V(0,i) <=> i-th vertex is not visited
+// Total DFS time: O(n^2/64)
 struct DenseDFS {
 	BitMatrix G, V; // space: O(n^2/64)
 
+	// Initialize structure for n vertices
 	DenseDFS(int n = 0) : G(n, n), V(1, n) {
 		reset();
 	}
 
+	// Mark all vertices as unvisited
 	void reset() { each(x, V.M) x = -1; }
+
+	// Get/set visited flag for i-th vertex
 	void setVisited(int i) { V.set(0, i, 0); }
 	bool isVisited(int i)  { return !V(0, i); }
 
 	// DFS step: func is called on each unvisited
 	// neighbour of i. You need to manually call
-	// setVisited(child) to mark it visited.
-	template<class T> // Single step: O(n/64)
+	// setVisited(child) to mark it visited
+	// or this function will call the callback
+	// with the same vertex again.
+	template<class T>
 	void step(int i, T func) {
 		ull* E = G.row(i);
 		for (int w = 0; w < G.stride;) {
--- src/math/bit_matrix.h
+++ src/math/bit_matrix.h
@@ -4,25 +4,29 @@
 using ull = uint64_t;
 
 // Matrix over Z_2 (bits and xor)
-// UNTESTED and UNFINISHED
+// TODO: arithmetic operations
 struct BitMatrix {
 	vector<ull> M;
 	int rows, cols, stride;
 
+	// Create matrix with n rows and m columns
 	BitMatrix(int n = 0, int m = 0) {
 		rows = n; cols = m;
 		stride = (m+63)/64;
 		M.resize(n*stride);
 	}
 
+	// Get pointer to bit-packed data of i-th row
 	ull* row(int i) { return &M[i*stride]; }
 
+	// Get value in i-th row and j-th column
 	bool operator()(int i, int j) {
 		return (row(i)[j/64] >> (j%64)) & 1;
 	}
 
+	// Set value in i-th row and j-th column
 	void set(int i, int j, bool val) {
-		ull &w = row(i)[j/64], m = 1 << (j%64);
+		ull &w = row(i)[j/64], m = 1ull << (j%64);
 		if (val) w |= m;
 		else w &= ~m;
 	}
