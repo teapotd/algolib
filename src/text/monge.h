c8d0779665790721171450112148ab445340ab42
teapotd <teapot404@gmail.com>
2022-05-09 19:40:10 +0200

    text/monge: Draft

---
+++ src/text/monge.h
@@ -0,0 +1,81 @@
+#pragma once
+#include "../template.h"
+
+// Invert subpermutation.
+// Missing values are set to `def`.
+Vi invert(const Vi& P, int def = -1) {
+	Vi ret(sz(P), def);
+	rep(i, 0, sz(P))
+		if (P[i] >= 0 && P[i] < sz(P))
+			ret[P[i]] = i;
+	return ret;
+}
+
+// Split permutation P into half `lo` with low
+// values and half `hi` with high values.
+// Missing rows from `lo` and `hi` are removed,
+// original indices are in `loInd` and `hiInd`.
+void split(const Vi& P, Vi& lo, Vi& hi,
+           Vi& loInd, Vi& hiInd) {
+  int i = 0;
+	each(e, P) {
+		if (e < sz(P)/2) lo.pb(e), loInd.pb(i++);
+		else hi.pb(e - sz(P)/2), hiInd.pb(i++);
+	}
+}
+
+// Expand permutation into subpermutation
+// of length `n` on given indices sets.
+Vi expand(const Vi& P, Vi& ind1, Vi& ind2,
+          int n, int def) {
+	Vi ret(n, def);
+	rep(k, 0, sz(P)) ret[ind1[k]] = ind2[P[k]];
+	return ret;
+}
+
+// Compute (min, +) product of square
+// simple unit-Monge matrices given their
+// permutation representations; O(n log n)
+//! Source: https://arxiv.org/pdf/0707.3619.pdf
+Vi mongeMul(const Vi& P, const Vi& Q) {
+	int n = sz(P);
+	if (n <= 1) return P;
+
+	Vi p1, p2, q1, q2, i1, i2, j1, j2;
+	split(P, p1, p2, i1, i2);
+	split(invert(Q), q1, q2, j1, j2);
+
+	p1 = mongeMul(p1, invert(q1));
+	p2 = mongeMul(p2, invert(q2));
+
+	p1 = expand(p1, i1, j1, n, -2);
+	p2 = expand(p2, i2, j2, n, n+1);
+	q1 = invert(p1, -2);
+	q2 = invert(p2, n+1);
+
+	Vi ans(n, -1);
+	int j = n, delta = 0;
+
+	rep(i, 0, n) {
+		while (j > 0 && delta >= 0) {
+			j--;
+			delta -= (q2[j] < i) + (q1[j] >= i);
+		}
+
+		if (delta < 0) {
+			if (p2[i] < j || p1[i] >= j) {
+				if (q2[j] <= i || q1[j] > i) {
+					ans[i] = j;
+				}
+			}
+		}
+
+		if (ans[i] == -1) {
+			ans[i] = (p1[i] != -2 ? p1[i] : p2[i]);
+		}
+
+		delta += (p2[i] < j) + (p1[i] >= j);
+	}
+
+	return ans;
+}
---
+++ tests/manual/text/monge.cpp
@@ -0,0 +1,90 @@
+#include "../../../src/text/monge.h"
+#include "../testing.h"
+
+vector<Vi> permToMongeMatrix(const Vi& perm) {
+	int n = sz(perm);
+	vector<Vi> mat(n+1, Vi(n+1));
+	for (int i = n-1; i >= 0; i--) {
+		for (int j = 1; j <= n; j++) {
+			mat[i][j] = mat[i+1][j] + mat[i][j-1] - mat[i+1][j-1] + (perm[i] == j-1);
+		}
+	}
+	return mat;
+}
+
+Vi mongeMatrixToPerm(const vector<Vi>& mat) {
+	int n = sz(mat)-1;
+	Vi perm(n);
+	rep(i, 0, n) rep(j, 1, n+1) {
+		int p = mat[i][j] - (mat[i+1][j] + mat[i][j-1] - mat[i+1][j-1]);
+		if (p) perm[i] = j-1;
+	}
+	return perm;
+}
+
+vector<Vi> distanceMul(const vector<Vi>& A, const vector<Vi>& B) {
+	vector<Vi> C(sz(A), Vi(sz(B[0])));
+	rep(i, 0, sz(A)) rep(j, 0, sz(B[0])) {
+		C[i][j] = INT_MAX;
+		rep(k, 0, sz(A[0])) {
+			C[i][j] = min(C[i][j], A[i][k]+B[k][j]);
+		}
+	}
+	return C;
+}
+
+Vi mongeMulNaive(const Vi& P, const Vi& Q) {
+	auto A = permToMongeMatrix(P);
+	auto B = permToMongeMatrix(Q);
+	auto C = distanceMul(A, B);
+	return mongeMatrixToPerm(C);
+}
+
+void testMongeMul(const Vi& P, const Vi& Q) {
+	Vi expected = mongeMulNaive(P, Q);
+	Vi got = mongeMul(P, Q);
+	if (got != expected) {
+		deb(P);
+		deb(Q);
+		deb(expected);
+		deb(got);
+		assert(0);
+	}
+}
+
+void testMongeMulAll(int n) {
+	Vi P(n);
+	iota(all(P), 0);
+	do {
+		Vi Q(n);
+		iota(all(Q), 0);
+		do {
+			testMongeMul(P, Q);
+		} while (next_permutation(all(Q)));
+	} while (next_permutation(all(P)));
+}
+
+int main() {
+	constexpr int MAX_LENGTH_ALL = 6;
+	constexpr int MAX_LENGTH_RAND = 100;
+
+	rep(n, 0, MAX_LENGTH_ALL+1) {
+		deb(n);
+		testMongeMulAll(n);
+	}
+
+	for (int cnt = 1;; cnt++) {
+		int n = r(MAX_LENGTH_ALL+1, MAX_LENGTH_RAND);
+		Vi P(n), Q(n);
+		iota(all(P), 0);
+		iota(all(Q), 0);
+		shuffle(all(P), rnd);
+		shuffle(all(Q), rnd);
+		testMongeMul(P, Q);
+		if (cnt%1000 == 0) {
+			deb(cnt);
+		}
+	}
+
+	return 0;
+}
