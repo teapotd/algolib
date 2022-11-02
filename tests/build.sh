f95f72cf6e79438f94b622fa0be422b7bc1e03bb
teapotd <4314728+teapotd@users.noreply.github.com>
2022-11-02 17:21:16 +0100

    Use macOS specific build options only on macOS

--- tests/build.sh
+++ tests/build.sh
@@ -3,22 +3,26 @@ set -e
 ROOT=`pwd`
 
 if [ $# -ne 2 ]; then
-    echo "usage: ./build.sh [b|d] [path to cpp file]"
-    exit 1
+	echo "usage: ./build.sh [b|d] [path to cpp file]"
+	exit 1
 fi
 
-FLAGS="-std=c++17 -fconcepts -DLOC -Wl,-stack_size -Wl,0x10000000"
+FLAGS="-std=c++17 -fconcepts -DLOC"
 FLAGS+=" -Wall -W -Wfatal-errors -Wshadow -Wlogical-op -Wconversion -Wfloat-equal"
 FLAGS+=" -Wformat=2 -Wcast-qual -Wcast-align -Wduplicated-cond"
 FLAGS+=" -Wno-misleading-indentation"
 
+if [[ $OSTYPE == 'darwin'* ]]; then
+	FLAGS+=" -Wl,-stack_size -Wl,0x10000000 -fvisibility=hidden"
+fi
+
 if [ $1 = "b" ]; then
 	FLAGS+=" -O2"
 elif [ $1 = "d" ]; then
-	FLAGS+=" -O0 -g -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address,undefined -fvisibility=hidden"
+	FLAGS+=" -O0 -g -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address,undefined"
 else
-    echo "usage: ./build.sh [b|d] [path to cpp file]"
-    exit 1
+	echo "usage: ./build.sh [b|d] [path to cpp file]"
+	exit 1
 fi
 
 SRC=$ROOT/$2
