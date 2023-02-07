b4dfa3d78e55f4f040520a9e075af898833ca6a6
teapotd <4314728+teapotd@users.noreply.github.com>
2023-02-07 13:00:47 +0100

    math/pi_large_precomp: Improvements

--- src/math/pi_large_precomp.h
+++ src/math/pi_large_precomp.h
@@ -4,48 +4,64 @@
 
 // Count primes in given interval
 // using precomputed table.
-// Set MAX_P to sqrt(MAX_N) and run sieve()!
+// Set MAX_P to sqrt(MAX_N)!
 // Precomputed table will contain N_BUCKETS
 // elements - check source size limit.
+// If you need to pack more values,
+// you can use `utils/packing.h`.
 
-constexpr ll MAX_N = 1e11+1;
-constexpr ll N_BUCKETS = 10000;
+// Precomputed table size:
+//   MAX_N=1e11, N_BUCKETS=1e4 -> 43.96 KB
+//   MAX_N=1e11, N_BUCKETS=2e4 -> 85.55 KB
+
+constexpr ll MAX_N = 1e11;
+constexpr ll N_BUCKETS = 2e4;
 constexpr ll BUCKET_SIZE = (MAX_N/N_BUCKETS)+1;
+#ifndef PRECOMPUTED_DEFINED //!HIDE
 constexpr ll precomputed[] = {/* ... */};
+#endif //!HIDE
 
-ll sieveRange(ll from, ll to) {
+// Unpack precomputed data.
+vector<ll> buckets = [] {
+	vector<ll> ret(N_BUCKETS+1);
+	ll d = 0;
+	rep(i, 0, N_BUCKETS)
+		ret[i+1] = ret[i] + (d += precomputed[i]);
+	return ret;
+}();
+
+// Count primes in range [b;e) naively.
+ll sieveRange(ll b, ll e) {
 	bitset<BUCKET_SIZE> elems;
-	from = max(from, 2LL);
-	to = max(from, to);
+	b = max(b, 2LL);
+	e = max(b, e);
 	each(p, primesList) {
-		ll c = max((from+p-1) / p, 2LL);
-		for (ll i = c*p; i < to; i += p)
-			elems.set(i-from);
+		ll c = max((b+p-1) / p, 2LL);
+		for (ll i = c*p; i < e; i += p)
+			elems.set(i-b);
 	}
-	return to-from-elems.count();
+	return e-b-elems.count();
 }
 
 // Run once on local computer to precompute
 // table. Takes about 10 minutes for n = 1e11.
-// Sanity check (for default params):
-// 664579, 606028, 587253, 575795, ...
+// First and last values for default params:
+// 348513, -32447, -9941, -6221, -4585,
+// ..., -162, -162, 563, -286, -949
 void localPrecompute() {
-	for (ll i = 0; i < MAX_N; i += BUCKET_SIZE) {
-		ll to = min(i+BUCKET_SIZE, MAX_N);
-		cout << sieveRange(i, to) << ',' << flush;
+	ll last = 0;
+	for (ll i = 0; i <= MAX_N; i += BUCKET_SIZE) {
+		ll to = min(i+BUCKET_SIZE, MAX_N+1);
+		ll cur = sieveRange(i, to);
+		cout << cur-last << ',' << flush;
+		last = cur;
 	}
 	cout << endl;
 }
 
-// Count primes in [from;to) using table.
-// O(N_BUCKETS + BUCKET_SIZE*lg lg n + sqrt(n))
-ll countPrimes(ll from, ll to) {
-	ll bFrom = from/BUCKET_SIZE+1,
-	   bTo = to/BUCKET_SIZE;
-	if (bFrom > bTo) return sieveRange(from, to);
-	ll ret = accumulate(precomputed+bFrom,
-	                    precomputed+bTo, 0);
-	ret += sieveRange(from, bFrom*BUCKET_SIZE);
-	ret += sieveRange(bTo*BUCKET_SIZE, to);
-	return ret;
+// Count number of primes <= x;
+// time: O(BUCKET_SIZE*lg lg n + sqrt(n)/lg(n))
+ll pi(ll x) {
+	ll b = x/BUCKET_SIZE, j = b*BUCKET_SIZE;
+	return buckets[b] + sieveRange(j, x+1);
 }
