fd528adfd0b6bc3507ca06d8b75ff3d319a86c17
teapotd <4314728+teapotd@users.noreply.github.com>
2023-08-10 23:01:37 +0200

    geo2d/segment: side, fix degenerate case in distTo

--- src/geo2d/segment.h
+++ src/geo2d/segment.h
@@ -14,13 +14,23 @@ struct seg {
 		       !sgn((a-p).cross(b-p));
 	}
 
+	// Returns 0 if point p lies on the line ab,
+	// 1 if to the left of the vector ab,
+	// -1 if on the right of the vector ab.
+	// Depends on vec: cross
+	int side(vec p) {
+		return sgn((b-a).cross(p-a));
+	}
+
 	// Distance from segment to point.
 	// Depends on vec: -, dot, cross, len
 	double distTo(vec p) const {
-		if ((p-a).dot(b-a) < 0) return (p-a).len();
-		if ((p-b).dot(a-b) < 0) return (p-b).len();
+		if (sgn((p-a).dot(b-a)) <= 0)
+			return (p-a).len();
+		if (sgn((p-b).dot(a-b)) <= 0)
+			return (p-b).len();
 		return double(abs((p-a).cross(b-a))) /
-		       (b-a).len();
+			(b-a).len();
 	}
 
 #if not FLOATING_POINT_GEOMETRY
@@ -28,9 +38,9 @@ struct seg {
 	// -1 if smaller, 0 if equal, 1 if greater
 	// Depends on vec: -, dot, cross, len2
 	int cmpDistTo(vec p, ll d2) const {
-		if ((p-a).dot(b-a) < 0)
+		if ((p-a).dot(b-a) <= 0)
 			return sgn((p-a).len2()-d2);
-		if ((p-b).dot(a-b) < 0)
+		if ((p-b).dot(a-b) <= 0)
 			return sgn((p-b).len2()-d2);
 		ll c = (p-a).cross(b-a);
 		return sgn(c*c - d2 * (b-a).len2());
