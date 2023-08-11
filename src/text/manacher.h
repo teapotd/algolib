8109cc16dbcd89e2da3df399eea44b201f32f3bc
teapotd <4314728+teapotd@users.noreply.github.com>
2023-08-11 09:22:11 +0200

    text/manacher: Replace buggy version with kactl

--- src/text/manacher.h
+++ src/text/manacher.h
@@ -3,20 +3,20 @@
 
 // Manacher algorithm; time: O(n)
 // Finds largest radiuses for palindromes:
-// r[2*i] = for center at i (single letter = 1)
-// r[2*i+1] = for center between i and i+1
-template<class T> vi manacher(const T& str) {
-	int n = sz(str)*2, c = 0, e = 1;
-	vi r(n, 1);
-	auto get = [&](int i) { return i%2 ? 0 :
-		(i >= 0 && i < n ? str[i/2] : i); };
-
-	rep(i, 0, n) {
-		if (i < e) r[i] = min(r[c*2-i], e-i);
-		while (get(i-r[i]) == get(i+r[i])) r[i]++;
-		if (i+r[i] > e) c = i, e = i+r[i]-1;
+// p[0][i] for center between i-1 and i
+// p[1][i] for center at i (single letter = 0)
+//! Source: https://github.com/kth-competitive-programming/kactl/blob/main/content/strings/Manacher.h
+template<class T>
+array<vi, 2> manacher(const T& s) {
+	int n = sz(s), l = 0, r = 0;
+	array<vi, 2> p = {vi(n+1), vi(n)};
+	rep(i, 0, n) rep(z, 0, 2) {
+		int t = r-i+!z, &x = p[z][i];
+		if (i < r) x = min(t, p[z][l+t]);
+		int b = i-x-1, e = i+x+z;
+		while (b >= 0 && e < n && s[b] == s[e])
+			x++, b--, e++;
+		if (r < e) l = b+1, r = e-1;
 	}
-
-	rep(i, 0, n) r[i] /= 2;
-	return r;
+	return p;
 }
--- tests/manual/text/manacher.cpp
+++ tests/manual/text/manacher.cpp
@@ -1,7 +1,7 @@
 #include "../../../src/text/manacher.h"
 #include "../testing.h"
 
-vi naiveManacher(const string& tmp) {
+vi naiveManacherOld(const string& tmp) {
 	string str;
 	each(c, tmp) {
 		str.pb(c);
@@ -23,24 +23,58 @@ vi naiveManacher(const string& tmp) {
 	return ret;
 }
 
-void test(int n, int alpha) {
-	string str;
+array<vi, 2> naiveManacher(const string& s) {
+	int n = sz(s);
+	array<vi, 2> p = {vi(n+1), vi(n)};
+	vi old = naiveManacherOld(s);
 	rep(i, 0, n) {
-		str.pb(char('a' + r(0, alpha-1)));
+		p[0][i+1] = old[i*2+1];
+		p[1][i] = old[i*2] - 1;
 	}
+	return p;
+}
 
-	vi naive = naiveManacher(str);
-	vi fast = manacher(str);
-	deb(naive, fast);
+void test(const string& str) {
+	auto naive = naiveManacher(str);
+	auto fast = manacher(str);
+	// deb(naive, fast);
 	assert(naive == fast);
 }
 
-int main() {
-	rep(alpha, 1, 10) rep(n, 1, 20) {
-		deb(n, alpha);
-		rep(i, 0, 10) {
-			test(n, alpha);
+void testRandom(int n, int alpha) {
+	string str;
+	rep(i, 0, n) str.pb(char('a' + r(0, alpha-1)));
+	test(str);
+}
+
+void testAll(int n, int alpha) {
+	string str(n, 'a');
+	bool ok = 1;
+	while (ok) {
+		test(str);
+		ok = 0;
+		rep(i, 0, n) {
+			if (++str[i] == char('a'+alpha)) {
+				str[i] = 'a';
+			} else {
+				ok = 1;
+				break;
+			}
 		}
 	}
+}
+
+int main() {
+	rep(n, 1, 20) {
+		testAll(n, 2);
+	}
+
+	while (true) {
+		int n = r(21, 100);
+		int alpha = r(2, 3);
+		testRandom(n, alpha);
+	}
+
+	// testRandom(1e6, 1);
 	return 0;
 }
