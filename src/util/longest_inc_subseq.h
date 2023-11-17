e093c8c51eae6223f4f0567c21bbf091d1538410
teapotd <4314728+teapotd@users.noreply.github.com>
2023-11-17 22:44:16 +0100

    util/longest_inc_subseq: Solution recovery

--- src/util/longest_inc_subseq.h
+++ src/util/longest_inc_subseq.h
@@ -2,9 +2,20 @@
 #include "../template.h"
 
 // Longest Increasing Subsequence; O(n lg n)
-int lis(const vi& seq) {
-	vi dp(sz(seq), INT_MAX);
-	each(c, seq) *lower_bound(all(dp), c) = c;
-	return int(lower_bound(all(dp), INT_MAX)
-	           - dp.begin());
+vi lis(const vi& seq) {
+	vi dp(sz(seq)+1, INT_MAX);
+	vi ind(sz(dp), -1), prv(sz(dp));
+
+	rep(i, 0, sz(seq)) {
+		int j = int(lower_bound(1+all(dp), seq[i])
+		            - dp.begin());
+		prv[i] = ind[j-1];
+		dp[j] = seq[ind[j] = i];
+	}
+
+	vi ret;
+	int i = *--find(1+all(ind), -1);
+	while (i != -1) ret.pb(i), i = prv[i];
+	reverse(all(ret));
+	return ret;
 }
