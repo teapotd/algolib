94f72a78314d63a7834ed69c00ac67359650368b
teapotd <4314728+teapotd@users.noreply.github.com>
2024-03-21 16:27:30 +0100

    trees/heavylight_decomp: Improvements

--- src/trees/heavylight_decomp.h
+++ src/trees/heavylight_decomp.h
@@ -5,110 +5,101 @@
 // Heavy-Light Decomposition of tree
 // with subtree query support; space: O(n)
 struct HLD {
-	// Subtree of v = [pos[v]; pos[v]+size[v])
-	// Chain with v = [chBegin[v]; chEnd[v])
-	vi par;     // Vertex parent
-	vi size;    // Vertex subtree size
-	vi depth;   // Vertex distance to root
-	vi pos;     // Vertex position in "HLD" order
-	vi chBegin; // Begin of chain with vertex
-	vi chEnd;   // End of chain with vertex
-	vi order;   // "HLD" preorder of vertices
-	SegTree tree; // Verts are in HLD order
+	// G[v] = children of v (no parents!)
+	// G[v][0] = heavy child of v
+	// par[v] = parent of vertex v
+	// size[v] = size of subtree rooted at v
+	// depth[v] = distance from root to v
+	// pos[v] = index of v in "HLD preorder"
+	// head[v] = first vertex of chain with v
+	// len[v] = length of chain starting at v
+	//          (0 if v is not head of chain)
+	// order[i] = i-th vertex in "HLD preorder"
+	vector<vi> G;
+	vi par, size, depth, pos, head, len, order;
+	SegTree tree; // Vertices are in HLD order
 
 	HLD() {}
 
 	// Initialize structure for tree G
-	// and given root; time: O(n lg n)
-	// MODIFIES ORDER OF EDGES IN G!
-	HLD(vector<vi>& G, int root)
-			: par(sz(G)), size(sz(G)),
-			  depth(sz(G)), pos(sz(G)),
-			  chBegin(sz(G)), chEnd(sz(G)) {
-		dfs(G, root, -1);
-		decomp(G, root, -1, 0);
+	// and given root v; time: O(n lg n)
+	HLD(vector<vi> H, int v)
+			: G(move(H)), par(sz(G), -1),
+			  size(sz(G), 1), depth(sz(G)),
+			  pos(sz(G)), head(sz(G)), len(sz(G)) {
+		dfs(v);
+		go(v, v);
 		tree = {sz(order)};
 	}
 
-	void dfs(vector<vi>& G, int v, int p) {
-		par[v] = p;
-		size[v] = 1;
-		depth[v] = p < 0 ? 0 : depth[p]+1;
-
-		if (G[v].empty()) return;
-		int& fs = G[v][0];
-		if (fs == p) swap(fs, G[v].back());
-
-		each(e, G[v]) if (e != p) {
-			dfs(G, e, v);
+	void dfs(int v) {
+		erase(G[v], par[v]);
+		each(e, G[v]) {
+			depth[e] = depth[par[e] = v] + 1;
+			dfs(e);
 			size[v] += size[e];
-			if (size[e] > size[fs]) swap(e, fs);
+			if (size[e] > size[G[v][0]])
+				swap(G[v][0], e);
 		}
 	}
 
-	void decomp(vector<vi>& G,
-	            int v, int p, int chb) {
+	void go(int v, int h) {
 		pos[v] = sz(order);
-		chBegin[v] = chb;
-		chEnd[v] = pos[v]+1;
+		len[head[v] = h]++;
 		order.pb(v);
-
-		each(e, G[v]) if (e != p) {
-			if (e == G[v][0]) {
-				decomp(G, e, v, chb);
-				chEnd[v] = chEnd[e];
-			} else {
-				decomp(G, e, v, sz(order));
-			}
-		}
+		each(e, G[v]) go(e, G[v][0] == e ? h : e);
 	}
-
-	// Get root of chain containing v
-	int chRoot(int v) {return order[chBegin[v]];}
 
 	// Level Ancestor Query; time: O(lg n)
 	int laq(int v, int level) {
-		while (true) {
-			int k = pos[v] - depth[v] + level;
-			if (k >= chBegin[v]) return order[k];
-			v = par[chRoot(v)];
+		for (;; v = par[v]) {
+			int k = level - depth[v = head[v]];
+			if (k >= 0) return order[pos[v]+k];
 		}
 	}
 
 	// Lowest Common Ancestor; time: O(lg n)
 	int lca(int a, int b) {
-		while (chBegin[a] != chBegin[b]) {
-			int ha = chRoot(a), hb = chRoot(b);
+		for (;;) {
+			int ha = head[a], hb = head[b];
+			if (ha == hb)
+				return depth[a] < depth[b] ? a : b;
 			if (depth[ha] > depth[hb]) a = par[ha];
 			else b = par[hb];
 		}
-		return depth[a] < depth[b] ? a : b;
 	}
 
-	// Call func(chBegin, chEnd) on each path
-	// segment; time: O(lg n * time of func)
+	// Call func(begin, end, isAscending)
+	// for each path segment in order
+	// from a to b; time: O(lg n * time of func)
+	// func can be called on empty intervals!
 	void iterPath(int a, int b, auto func) {
-		while (chBegin[a] != chBegin[b]) {
-			int ha = chRoot(a), hb = chRoot(b);
+		for (static vector<pii> tmp;;) {
+			int ha = head[a], hb = head[b];
+			if (ha == hb) {
+				bool f = (pos[a] > pos[b]);
+				if (f) swap(a, b);
+				// Remove +1 from pos[a]+1 for vertex
+				// queries (with +1 -> edges).
+				func(pos[a]+1, pos[b]+1, !f);
+				reverse(all(tmp));
+				each(e, tmp) func(e.x, e.y, 0);
+				return tmp.clear();
+			}
 			if (depth[ha] > depth[hb]) {
-				func(chBegin[a], pos[a]+1);
+				func(pos[ha], pos[a]+1, 1);
 				a = par[ha];
 			} else {
-				func(chBegin[b], pos[b]+1);
+				tmp.pb({pos[hb], pos[b]+1});
 				b = par[hb];
 			}
 		}
-
-		if (pos[a] > pos[b]) swap(a, b);
-		// Remove +1 from pos[a]+1 for vertices
-		// queries (with +1 -> edges).
-		func(pos[a]+1, pos[b]+1);
 	}
 
 	// Query path between a and b; O(lg^2 n)
 	SegTree::T queryPath(int a, int b) {
 		auto ret = tree.ID;
-		iterPath(a, b, [&](int i, int j) {
+		iterPath(a, b, [&](int i, int j, bool) {
 			ret = tree.f(ret, tree.query(i, j));
 		});
 		return ret;
