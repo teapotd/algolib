d30f4f3ffff3964b064ec7905525a57a301edf52
teapotd <4314728+teapotd@users.noreply.github.com>
2024-04-02 18:08:57 +0200

    geo2d/circle: Tangents

--- src/geo2d/circle.h
+++ src/geo2d/circle.h
@@ -20,13 +20,13 @@ struct circle {
 #if FLOATING_POINT_GEOMETRY
 	// Intersect with another circle.
 	// Returns number of intersection points
-	// (greater than 2 means infinity).
+	// (3 means circles are identical).
 	// Arc is CCW w.r.t to `this`, CW for `a`.
 	// Depends on vec: +, -, *, len2, perp
 	int intersect(circle a, pair<vec,vec>& out) {
 		vec d = a.p - p;
 		auto d2 = d.len2();
-		if (!sgn(d2)) return !sgn(r2-a.r2) ? 3 : 0;
+		if (!sgn(d2)) return sgn(r2-a.r2) ? 0 : 3;
 		auto pd = (d2 + r2 - a.r2) / 2;
 		auto h2 = r2 - pd*pd / d2;
 		vec h, t = p + d*(pd/d2);
@@ -37,7 +37,8 @@ struct circle {
 	}
 
 	// Intersect with line.
-	// Returns number of intersection points
+	// Returns number of intersection points.
+	// Points are in order given by a.v.perp().
 	int intersect(line a, pair<vec, vec>& out) {
 		auto d = a.distTo(p), h2 = r2 - d*d;
 		vec h, t = a.proj(p);
@@ -45,6 +46,40 @@ struct circle {
 		if (s > 1)
 			h = a.v.perp() * sqrt(h2 / a.v.len2());
 		out = {t-h, t+h};
+		return s;
+	}
+
+	// Find normal vectors of tangents.
+	// Returns number of tangent points
+	// (3 means circle is degenerated to `a`).
+	// Covered arc is CCW between vectors.
+	int tangents(vec a, pair<vec, vec>& out) {
+		vec d = a - p;
+		auto d2 = d.len2(), h2 = d2 - r2;
+		if (!sgn(d2)) return sgn(h2) ? 0 : 3;
+		vec h, t = d * sqrt(r2);
+		int s = sgn(h2)+1;
+		if (s > 1) h = d.perp() * sqrt(h2);
+		out = {(t-h)/d2, (t+h)/d2};
+		return s;
+	}
+
+	// Find normal vectors of tangents.
+	// Returns number of tangent points
+	// (3 means circles are identical).
+	// Arc for `this` is CCW between vectors.
+	// For `a`, it is CW for outer, CCW for inner
+	int tangents(circle a, bool inner,
+	             pair<vec, vec>& out) {
+		vec d = a.p - p;
+		auto d2 = d.len2();
+		auto dr = sqrt(r2)+sqrt(a.r2)*(inner*2-1);
+		auto h2 = d2 - dr*dr;
+		if (!sgn(d2)) return sgn(h2) ? 0 : 3;
+		vec h, t = d * dr;
+		int s = sgn(h2)+1;
+		if (s > 1) h = d.perp() * sqrt(h2);
+		out = {(t-h)/d2, (t+h)/d2};
 		return s;
 	}
 #endif
--- tests/own/geo2d/circle.hpp
+++ tests/own/geo2d/circle.hpp
@@ -3,6 +3,7 @@
 #include "common.hpp"
 
 #if FLOATING_POINT_GEOMETRY
+
 vec between(vec p, vec a, vec b) {
 	double x = (a-p).angle(), y = (b-p).angle();
 	if (x > y) y += M_PI*2;
@@ -12,16 +13,16 @@ vec between(vec p, vec a, vec b) {
 void circleCircle(circle a, circle b, int expected) {
 	pair<vec, vec> out;
 	assert(a.intersect(b, out) == expected);
+	if (expected < 1 || expected > 2) return;
+
+	assert(a.side(out.x) == 0 && b.side(out.x) == 0);
+	assert(a.side(out.y) == 0 && b.side(out.y) == 0);
 
 	if (expected == 1) {
 		assert(out.x == out.y);
-		assert(a.side(out.x) == 0 && b.side(out.x) == 0);
 	} else if (expected == 2) {
 		assert(out.x != out.y);
-		assert(a.side(out.x) == 0 && b.side(out.x) == 0);
-		assert(a.side(out.y) == 0 && b.side(out.y) == 0);
-
-		// Test if arc is oriented CCW for a and CW for b.
+		// Test if covered arc is oriented CCW for a and CW for b.
 		assert(b.side(between(a.p, out.x, out.y)) == 1);
 		assert(a.side(between(b.p, out.y, out.x)) == 1);
 	}
@@ -30,19 +31,102 @@ void circleCircle(circle a, circle b, int expected) {
 void circleLine(circle a, line b, int expected) {
 	pair<vec, vec> out;
 	assert(a.intersect(b, out) == expected);
+	if (expected < 1 || expected > 2) return;
+
+	assert(a.side(out.x) == 0 && b.side(out.x) == 0);
+	assert(a.side(out.y) == 0 && b.side(out.y) == 0);
 
 	if (expected == 1) {
 		assert(out.x == out.y);
-		assert(a.side(out.x) == 0 && b.side(out.x) == 0);
 	} else if (expected == 2) {
 		assert(out.x != out.y);
-		assert(a.side(out.x) == 0 && b.side(out.x) == 0);
-		assert(a.side(out.y) == 0 && b.side(out.y) == 0);
-
-		// Test if points are ordered according to norm.perp()
+		// Test if points are ordered along b.v.perp().
 		assert(b.v.cross(out.x) < b.v.cross(out.y));
 	}
 }
+
+void tangentCirclePoint(circle a, vec b, int expected) {
+	pair<vec, vec> out;
+	assert(a.tangents(b, out) == expected);
+	if (expected < 1 || expected > 2) return;
+
+	double r = sqrt(a.r2);
+	vec q1 = a.p + out.x*r, q2 = a.p + out.y*r;
+
+	assert(!a.side(q1) && !a.side(q2));
+	assert(!sgn((q1-b).dot(out.x)));
+	assert(!sgn((q2-b).dot(out.y)));
+
+	if (expected == 1) {
+		assert(out.x == out.y);
+	} else if (expected == 2) {
+		assert(out.x != out.y);
+		// Test if covered arc is oriented CCW.
+		vec c = between(a.p, q1, q2);
+		assert((c-b).len() < (q1-b).len());
+	}
+}
+
+void tangentCircleCircleOuter(circle a, circle b, int expected) {
+	pair<vec, vec> out;
+	assert(a.tangents(b, false, out) == expected);
+	if (expected < 1 || expected > 2) return;
+
+	double ra = sqrt(a.r2), rb = sqrt(b.r2);
+	vec qa1 = a.p + out.x*ra, qa2 = a.p + out.y*ra;
+	vec qb1 = b.p + out.x*rb, qb2 = b.p + out.y*rb;
+
+	assert(!a.side(qa1) && !a.side(qa2) && !b.side(qb1) && !b.side(qb2));
+	assert(!sgn((qa1-qb1).dot(out.x)));
+	assert(!sgn((qa2-qb2).dot(out.y)));
+
+	if (expected == 1) {
+		assert(out.x == out.y);
+	} else if (expected == 2) {
+		assert(out.x != out.y);
+
+		// Test if covered arc is oriented CCW for a and CW for b.
+		vec c = between(a.p, qa1, qa2);
+		assert((c-b.p).len() < (qa1-b.p).len());
+		vec d = between(b.p, qb2, qb1);
+		assert((d-a.p).len() < (qb1-a.p).len());
+	}
+}
+
+void tangentCircleCircleInner(circle a, circle b, int expected) {
+	pair<vec, vec> out;
+	assert(a.tangents(b, true, out) == expected);
+	if (expected < 1 || expected > 2) return;
+
+	double ra = sqrt(a.r2), rb = sqrt(b.r2);
+	vec qa1 = a.p + out.x*ra, qa2 = a.p + out.y*ra;
+	vec qb1 = b.p - out.x*rb, qb2 = b.p - out.y*rb;
+
+	assert(!a.side(qa1) && !a.side(qa2) && !b.side(qb1) && !b.side(qb2));
+	assert(!sgn((qa1-qb1).dot(out.x)));
+	assert(!sgn((qa2-qb2).dot(out.y)));
+
+	if (expected == 1) {
+		assert(out.x == out.y);
+	} else if (expected == 2) {
+		assert(out.x != out.y);
+
+		// Test if covered arc is oriented CCW for a and CCW for b.
+		vec c = between(a.p, qa1, qa2);
+		assert((c-b.p).len() < (qa1-b.p).len());
+		vec d = between(b.p, qb1, qb2);
+		assert((d-a.p).len() < (qb1-a.p).len());
+	}
+}
+
+void tangentCircleCircle(circle a, circle b, int expectedOuter, int expectedInner) {
+	for (int i = 0; i < 2; i++) {
+		tangentCircleCircleOuter(a, b, expectedOuter);
+		tangentCircleCircleInner(a, b, expectedInner);
+		swap(a, b);
+	}
+}
+
 #endif
 
 void deterministic() {
@@ -71,6 +155,17 @@ void deterministic() {
 	circleLine({{6*U,3*U},16*U*U}, through({2*U,7*U},{2*U,-2*U}), 1);
 	circleLine({{6*U,3*U},16*U*U}, through({3*U,7*U},{5*U,-2*U}), 2);
 	circleLine({{6*U,3*U},16*U*U}, through({5*U,-2*U},{3*U,7*U}), 2);
+
+	tangentCirclePoint({{2*U,1*U},49*U*U}, {12*U,9*U}, 2);
+	tangentCirclePoint({{2*U,1*U},49*U*U}, {9*U,1*U}, 1);
+	tangentCirclePoint({{2*U,1*U},49*U*U}, {8*U,1*U}, 0);
+
+	tangentCircleCircle({{2*U,1*U},49*U*U}, {{18*U,6*U},{36*U*U}}, 2, 2);
+	tangentCircleCircle({{2*U,1*U},49*U*U}, {{10*U,-4*U},{64*U*U}}, 2, 0);
+	tangentCircleCircle({{2*U,1*U},49*U*U}, {{6*U,4*U},{9*U*U}}, 2, 0);
+	tangentCircleCircle({{14*U,-18*U},36*U*U}, {{18*U,-18*U},{4*U*U}}, 1, 0);
+	tangentCircleCircle({{14*U,-18*U},36*U*U}, {{14*U,-10*U},{4*U*U}}, 2, 1);
+	tangentCircleCircle({{4*U,2*U},25*U*U}, {{5*U,3*U},9*U*U}, 0, 0);
 #endif
 }
 
@@ -93,6 +188,7 @@ void fuzz() {
 		}
 
 		circle c1(p1, r1*r1), c2(p2, r2*r2);
+		tangentCirclePoint(c1, p2, sgn(d-r1)+1);
 		if (randBool()) swap(c1, c2);
 		circleCircle(c1, c2, expected);
 	}
